b881545bf95d3b0d7529b5c0373e929b24fc6b18
yangxingyuan <yangxingyuan@bytedance.com>
2023-08-09 17:38:11 +0800

    feat: add basic test case

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   root: true,
   extends: ['@modern-js'],
-  ignorePatterns: ['packages/cli/bin/rspress.js'],
+  ignorePatterns: [
+    'packages/cli/bin/rspress.js',
+    'vitest.config.ts',
+    '**/*.test.ts',
+  ],
 };
---
+++ packages/cli/tests/config/config-basic.test.ts
@@ -0,0 +1,7 @@
+import { describe, test, expect } from 'vitest';
+
+describe('Should load config file', () => {
+  test('Load rspress.config.ts', () => {
+    expect(true).toBe(true);
+  });
+});
