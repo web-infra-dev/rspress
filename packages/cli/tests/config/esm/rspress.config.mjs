e83d8b55d19c0240a564b267b08b97a06962c3ee
yangxingyuan <yangxingyuan@bytedance.com>
2023-08-16 14:52:52 +0800

    fix: test

--- packages/cli/tests/config/esm/rspress.config.mjs
+++ packages/cli/tests/config/esm/rspress.config.mjs
@@ -1,8 +1,6 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default {
   root: __dirname,
   title: 'my-title',
