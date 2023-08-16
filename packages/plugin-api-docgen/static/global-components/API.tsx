1e753a82d30c1e70f511d3212d198a807da25889
yangxingyuan <yangxingyuan@bytedance.com>
2023-08-16 16:52:26 +0800

    feat: add api-docgen-plugin

---
+++ packages/plugin-api-docgen/static/global-components/API.tsx
@@ -0,0 +1,23 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { getCustomMDXComponent } from '@rspress/core/theme';
+import { usePageData, useLang } from '@rspress/core/runtime';
+import './API.scss';
+
+export default (props: { moduleName: string }) => {
+  const lang = useLang();
+  const { page } = usePageData();
+  const { moduleName } = props;
+  // some api doc have two languages.
+  const apiDocMap = page.apiDocMap as Record<string, string>;
+  const apiDoc = apiDocMap[moduleName] || apiDocMap[`${moduleName}-${lang}`];
+  return (
+    <ReactMarkdown
+      remarkPlugins={[[remarkGfm]]}
+      components={getCustomMDXComponent() as Record<string, React.ElementType>}
+      skipHtml={true}
+    >
+      {apiDoc}
+    </ReactMarkdown>
+  );
+};
