59b34dad0ea8c8e4dc26adc6087120dc306553a7
10Derozan <fupeng.antik@gmail.com>
2023-08-30 16:27:54 +0800

    feat: init modern plugin about rspress (#28)
    Co-authored-by: yangxingyuan <39261479+sanyuan0704@users.noreply.github.com>

---
+++ packages/modern-plugin-rspress/modern.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, moduleTools } from '@modern-js/module-tools';
+
+// https://modernjs.dev/module-tools/en/api
+export default defineConfig({
+  buildConfig: {
+    buildType: 'bundle',
+    format: 'cjs',
+    target: 'es2020',
+    sourceMap: true,
+  },
+  plugins: [moduleTools()],
+});
