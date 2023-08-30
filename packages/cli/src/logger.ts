00d7959f5f5c3c9cc2b1bdb4b83ee0fcf792aea0
yangxingyuan <yangxingyuan@bytedance.com>
2023-08-30 16:55:27 +0800

    feat: optimize logger

--- packages/cli/src/logger.ts
+++ packages/cli/src/logger.ts
@@ -1,27 +1,27 @@
 import chalk from 'chalk';
 import type { Logger } from '@rspress/core';
 
-export const rspressMark = chalk.magenta.bold('[Rspress]');
+export const rspressMark = chalk.cyan('[Rspress]');
 
 export const logger: Logger = {
   info(msg: string) {
-    console.log(`${rspressMark} ${msg}`);
+    console.log(`${chalk.cyan.bold('[info]')} ${msg}`);
   },
   error(msg: string) {
-    console.log(`${rspressMark} ${chalk.red(msg)}`);
+    console.log(`${chalk.red('[error]')} ${msg}`);
   },
   warn(msg: string) {
-    console.log(`${rspressMark} ${chalk.yellow(msg)}`);
+    console.log(`${chalk.yellow('[warn]')} ${msg}`);
   },
   success(msg: string) {
-    console.log(`${rspressMark} ${chalk.green(msg)}`);
+    console.log(`${chalk.green('[success]')} ${msg}`);
   },
   debug(msg: string) {
     if (process.env.DEBUG) {
-      console.log(`${rspressMark} ${chalk.grey(msg)}`);
+      console.log(`${chalk.gray('[rspress:debug]')} ${msg}`);
     }
   },
   log(msg: string) {
-    console.log(`${rspressMark} ${msg}`);
+    console.log(`${chalk.cyan('[rspress]')} ${msg}`);
   },
 };
