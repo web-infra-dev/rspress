ea9fde76136513551aaaa4041c4db793fad57caf
yangxingyuan <yangxingyuan@bytedance.com>
2023-09-06 11:10:25 +0800

    chore:optimzie sidebar group active style

--- packages/core/src/theme-default/components/Sidebar/index.tsx
+++ packages/core/src/theme-default/components/Sidebar/index.tsx
@@ -1,272 +1,45 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { NormalizedSidebarGroup, SidebarItem } from '@rspress/shared';
-import { matchRoutes, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import {
+  NormalizedSidebarGroup,
+  SidebarItem as ISidebarItem,
+} from '@rspress/shared';
+import { matchRoutes } from 'react-router-dom';
 import { routes } from 'virtual-routes';
-import { Link } from '../Link';
 import { isActive } from '../../logic';
-import ArrowRight from '../../assets/arrow-right.svg';
-import { Tag } from '../Tag';
 import styles from './index.module.scss';
-import { removeBase, normalizeHref, withBase, usePageData } from '@/runtime';
+import { SidebarItem } from './SidebarItem';
+import { removeBase, usePageData } from '@/runtime';
+
+export interface SidebarItemProps {
+  id: string;
+  item: ISidebarItem | NormalizedSidebarGroup;
+  depth: number;
+  activeMatcher: (link: string) => boolean;
+  collapsed?: boolean;
+  setSidebarData: React.Dispatch<
+    React.SetStateAction<(NormalizedSidebarGroup | ISidebarItem)[]>
+  >;
+  preloadLink: (link: string) => void;
+}
 
 interface Props {
   isSidebarOpen?: boolean;
   pathname: string;
   langRoutePrefix: string;
-  sidebarData: (NormalizedSidebarGroup | SidebarItem)[];
+  sidebarData: (NormalizedSidebarGroup | ISidebarItem)[];
 }
 
-const highlightTitleStyle = {
+export const highlightTitleStyle = {
   color: 'var(--rp-c-text-1)',
   fontSize: '14px',
   paddingLeft: '24px',
   fontWeight: 'bold',
 };
 
-let matchCache: WeakMap<NormalizedSidebarGroup | SidebarItem, boolean> =
+// Note: the cache object won't be reassign in other module
+// eslint-disable-next-line import/no-mutable-exports
+export let matchCache: WeakMap<NormalizedSidebarGroup | ISidebarItem, boolean> =
   new WeakMap();
-
-interface SidebarItemProps {
-  id: string;
-  item: SidebarItem | NormalizedSidebarGroup;
-  depth: number;
-  activeMatcher: (link: string) => boolean;
-  collapsed?: boolean;
-  setSidebarData: React.Dispatch<
-    React.SetStateAction<(NormalizedSidebarGroup | SidebarItem)[]>
-  >;
-  preloadLink: (link: string) => void;
-}
-
-export function SidebarItemComp(props: SidebarItemProps) {
-  const { item, depth = 0, activeMatcher, id, setSidebarData } = props;
-  const active = item.link && activeMatcher(item.link);
-
-  const ref = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (active) {
-      ref.current?.scrollIntoView({
-        block: 'center',
-      });
-    }
-  }, []);
-
-  const { text } = item;
-
-  if ('items' in item) {
-    return (
-      <SidebarGroupComp
-        id={id}
-        key={`${item.text}-${id}`}
-        item={item}
-        depth={depth}
-        activeMatcher={activeMatcher}
-        collapsed={item.collapsed}
-        setSidebarData={setSidebarData}
-        preloadLink={props.preloadLink}
-      />
-    );
-  } else {
-    return (
-      <Link href={normalizeHref(item.link)} className={styles.menuLink}>
-        <div
-          ref={ref}
-          onMouseEnter={() => props.preloadLink(item.link)}
-          className={`${
-            active ? styles.menuItemActive : styles.menuItem
-          } mt-1 py-2 px-3 font-medium flex`}
-          style={{
-            // The first level menu item will have the same font size as the sidebar group
-            fontSize: depth === 0 ? '14px' : '13px',
-            marginLeft: depth === 0 ? 0 : '12px',
-            borderRadius: '0 1rem 1rem 0',
-            ...(depth === 0 ? highlightTitleStyle : {}),
-            ...(depth === 0 && active ? { color: 'var(--rp-c-brand)' } : {}),
-          }}
-        >
-          <Tag tag={item.tag} />
-          {text}
-        </div>
-      </Link>
-    );
-  }
-}
-
-export function SidebarGroupComp(props: SidebarItemProps) {
-  const { item, depth = 0, activeMatcher, id, setSidebarData } = props;
-  const navigate = useNavigate();
-  const isGroupMatched = matchCache.get(item);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const transitionRef = useRef<any>(null);
-  const innerRef = useRef<HTMLDivElement>(null);
-  const initialRender = useRef(true);
-  const initialState = useRef((item as NormalizedSidebarGroup).collapsed);
-  const active = item.link && activeMatcher(item.link);
-  const { collapsed, collapsible = true } = item as NormalizedSidebarGroup;
-  const collapsibleIcon = (
-    <div
-      style={{
-        cursor: 'pointer',
-        transition: 'transform 0.2s ease-out',
-        transform: collapsed ? 'rotate(0deg)' : 'rotate(90deg)',
-      }}
-    >
-      <ArrowRight />
-    </div>
-  );
-
-  useEffect(() => {
-    if (initialRender.current) {
-      return;
-    }
-
-    if (!containerRef.current || !innerRef.current) {
-      return;
-    }
-
-    if (transitionRef.current) {
-      clearTimeout(transitionRef.current);
-    }
-
-    const container = containerRef.current;
-    const inner = innerRef.current;
-    // We should add the margin-top(4px) of first item in list, which is a part of the height of the container
-    const contentHeight = inner.clientHeight + 4;
-    if (collapsed) {
-      container.style.maxHeight = `${contentHeight}px`;
-      container.style.transitionDuration = '0.5s';
-      inner.style.opacity = '0';
-
-      transitionRef.current = setTimeout(() => {
-        if (containerRef.current) {
-          containerRef.current.style.maxHeight = '0px';
-        }
-      }, 0);
-    } else {
-      container.style.maxHeight = `${contentHeight}px`;
-      container.style.transitionDuration = '0.3s';
-      inner.style.opacity = '1';
-
-      transitionRef.current = setTimeout(() => {
-        if (containerRef.current) {
-          containerRef.current.style.removeProperty('max-height');
-        }
-      }, 300);
-    }
-  }, [collapsed]);
-
-  useEffect(() => {
-    initialRender.current = false;
-  }, []);
-
-  const toggleCollapse: React.MouseEventHandler<HTMLDivElement> = (e): void => {
-    e.stopPropagation();
-    // update collapsed state
-    setSidebarData(sidebarData => {
-      const newSidebarData = [...sidebarData];
-      const indexes = id.split('-').map(Number);
-      const initialIndex = indexes.shift()!;
-      const root = newSidebarData[initialIndex];
-      let current = root;
-      for (const index of indexes) {
-        current = (current as NormalizedSidebarGroup).items[index];
-      }
-      if ('items' in current) {
-        current.collapsed = !current.collapsed;
-      }
-      return newSidebarData;
-    });
-  };
-
-  return (
-    <section
-      key={id}
-      className="mt-1 block"
-      style={{
-        marginLeft: depth === 0 ? 0 : '20px',
-      }}
-    >
-      <div
-        className={`flex justify-between items-center rounded-xl ${
-          isGroupMatched ? styles.menuItemActive : ''
-        } ${
-          // eslint-disable-next-line no-nested-ternary
-          active
-            ? styles.menuItemActive
-            : collapsible || item.link
-            ? styles.menuItem
-            : styles.menuItemStatic
-        }`}
-        onMouseEnter={() => item.link && props.preloadLink(item.link)}
-        onClick={e => {
-          if (item.link) {
-            navigate(withBase(normalizeHref(item.link)));
-            collapsed && toggleCollapse(e);
-          } else {
-            collapsible && toggleCollapse(e);
-          }
-        }}
-        style={{
-          borderRadius: depth === 0 ? '0 1rem 1rem 0' : undefined,
-          cursor: collapsible || item.link ? 'pointer' : 'normal',
-        }}
-      >
-        <h2
-          className="py-2.5 px-2 text-sm font-medium flex"
-          style={{
-            ...(depth === 0 ? highlightTitleStyle : {}),
-            ...(depth === 0 && isGroupMatched
-              ? {
-                  color: 'var(--rp-c-brand)',
-                }
-              : {}),
-          }}
-        >
-          <Tag tag={item.tag} />
-          {item.text}
-        </h2>
-        {collapsible && (
-          <div
-            className={`${styles.collapseContainer} p-2 rounded-xl`}
-            onClick={toggleCollapse}
-          >
-            {collapsibleIcon}
-          </div>
-        )}
-      </div>
-      <div
-        ref={containerRef}
-        className="transition-all duration-300 ease-in-out"
-        style={{
-          overflow: 'hidden',
-          maxHeight: initialState.current ? 0 : undefined,
-        }}
-      >
-        <div
-          ref={innerRef}
-          className="rspress-sidebar-group transition-opacity duration-500 ease-in-out"
-          style={{
-            opacity: initialState.current ? 0 : 1,
-            marginLeft: depth === 0 ? '14px' : 0,
-          }}
-        >
-          {(item as NormalizedSidebarGroup)?.items?.map((item, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <div key={index}>
-              <SidebarItemComp
-                {...props}
-                item={item}
-                depth={depth + 1}
-                id={`${id}-${index}`}
-                preloadLink={props.preloadLink}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-}
 
 export function SideBar(props: Props) {
   const {
@@ -282,7 +55,7 @@ export function SideBar(props: Props) {
   const hideNavbar =
     frontmatter?.hideNavbar ?? themeConfig?.hideNavbar ?? false;
   const [sidebarData, setSidebarData] = useState<
-    (SidebarItem | NormalizedSidebarGroup)[]
+    (ISidebarItem | NormalizedSidebarGroup)[]
   >(rawSidebarData.filter(Boolean).flat());
   const pathname = decodeURIComponent(rawPathname);
   useEffect(() => {
@@ -292,8 +65,8 @@ export function SideBar(props: Props) {
     // 1. Update sidebarData when pathname changes
     // 2. For current active item, expand its parent group
     // Cache, Avoid redundant calculation
-    matchCache = new WeakMap<NormalizedSidebarGroup | SidebarItem, boolean>();
-    const match = (item: NormalizedSidebarGroup | SidebarItem) => {
+    matchCache = new WeakMap<NormalizedSidebarGroup | ISidebarItem, boolean>();
+    const match = (item: NormalizedSidebarGroup | ISidebarItem) => {
       if (matchCache.has(item)) {
         return matchCache.get(item);
       }
@@ -311,7 +84,7 @@ export function SideBar(props: Props) {
       matchCache.set(item, false);
       return false;
     };
-    const traverse = (item: NormalizedSidebarGroup | SidebarItem) => {
+    const traverse = (item: NormalizedSidebarGroup | ISidebarItem) => {
       if ('items' in item) {
         item.items.forEach(traverse);
         if (match(item)) {
@@ -359,8 +132,8 @@ export function SideBar(props: Props) {
         >
           <nav className="pb-2">
             {sidebarData.map(
-              (item: NormalizedSidebarGroup | SidebarItem, index: number) => (
-                <SidebarItemComp
+              (item: NormalizedSidebarGroup | ISidebarItem, index: number) => (
+                <SidebarItem
                   id={String(index)}
                   item={item}
                   depth={0}
