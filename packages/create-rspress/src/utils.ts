a0183bddd8997e4b6816e468d82327e6ea953571
Evan Liu <55378595+evanryuu@users.noreply.github.com>
2023-09-08 10:26:54 +0800

    feat: improve starter and interactions (#72)
    * feat(create-rspress): improve starter and interactions
    
    * chore(create-rspress): add icon in config

--- packages/create-rspress/src/utils.ts
+++ packages/create-rspress/src/utils.ts
@@ -1,5 +1,7 @@
 import path from 'path';
 import fs from 'fs-extra';
+import prompts from 'prompts';
+import type { CustomPromptObject } from './types';
 
 export async function copyFolder(src: string, dest: string) {
   const renameFiles: Record<string, string> = {
@@ -21,6 +23,59 @@ export async function copyFolder(src: string, dest: string) {
   }
 }
 
+/**
+ * replace <%= data %> with variables
+ *
+ * @example
+ * ```ts
+ * let str = 'The value of <%= hello %> is <%= hello %>.'
+ * const data = {
+ *   hello: 'hello world'
+ * };
+ *
+ * console.log(replaceVar(str, data)); // "The value of hello world is hello world."
+ *
+ * ```
+ *
+ */
+export function replaceVar(
+  str: string,
+  data: Record<string, any>,
+  decorator = '',
+) {
+  return str.replace(/<%=\s*(.*?)\s*%>/g, (_, key) => {
+    const value = data[key.trim()];
+    return `${decorator}${value}${decorator}`;
+  });
+}
+
+/**
+ * read file contents
+ *
+ * replace <%= data %> within the file
+ *
+ * output the file to targetRoot
+ */
+export async function renderFile(
+  fileSrc: string,
+  templateDir: string,
+  targetRoot: string,
+  data: Record<string, any>,
+) {
+  const templateFilePath = path.resolve(templateDir, fileSrc);
+  const targetPath = path.resolve(targetRoot, fileSrc);
+
+  const file = await fs.readFile(templateFilePath, 'utf-8');
+  // in js file, string should be wrapped by single quote
+  const decorator = templateFilePath.endsWith('.ts') ? "'" : '';
+  const output = replaceVar(file, data, decorator);
+
+  if (!fs.existsSync(targetRoot)) {
+    await fs.mkdir(targetRoot, { recursive: true });
+  }
+  await fs.writeFile(targetPath, output);
+}
+
 export function getPkgManager() {
   const ua = process.env.npm_config_user_agent;
   if (!ua) {
@@ -36,4 +91,23 @@ export function getPkgManager() {
  */
 export function formatTargetDir(targetDir: string) {
   return targetDir.trim().replace(/\/+$/g, '');
+}
+
+/**
+ * custom prompt function
+ */
+export function initSitePrompts(options: CustomPromptObject[]) {
+  const initOptionEvent = (option: CustomPromptObject): CustomPromptObject => {
+    option.onState = state => {
+      const value = state.value || option.initial;
+      option.value = option.formatFn?.(value) || value;
+    };
+
+    return option;
+  };
+
+  // init formatFn and assign value
+  options.forEach(initOptionEvent);
+
+  return prompts(options);
 }
---
+++ packages/create-rspress/template/docs/hello.md
@@ -0,0 +1,4 @@
+# Hello World!
+
+## Start
+Write something to build your own docs! 🎁
