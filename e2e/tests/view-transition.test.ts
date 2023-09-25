81b335ef45018987a070edd0ae6834f1e073f102
zoolsher <zoolsher@gmail.com>
2023-09-25 18:00:05 +0800

    feat: add test case for transition with animation

---
+++ e2e/tests/view-transition.test.ts
@@ -0,0 +1,42 @@
+import { expect, test } from '@playwright/test';
+import path from 'path';
+import { getPort, killProcess, runDevCommand } from '../utils/runCommands';
+
+const fixtureDir = path.resolve(__dirname, '../fixtures');
+
+function getPendingPromise() {
+  let resolve;
+  let promise = new Promise(r => {
+    resolve = r;
+  });
+  return [promise, resolve];
+}
+
+test.describe('basic test', async () => {
+  let appPort;
+  let app;
+  test.beforeAll(async () => {
+    const appDir = path.join(fixtureDir, 'view-transition');
+    appPort = await getPort();
+    app = await runDevCommand(appDir, appPort);
+  });
+
+  test.afterAll(async () => {
+    if (app) {
+      await killProcess(app);
+    }
+  });
+
+  test.only('Navigation with animation', async ({ page }) => {
+    await page.goto(`http://localhost:${appPort}/guide`);
+    const session = await page.context().newCDPSession(page);
+    await session.send('Animation.enable');
+    const [end, resolve] = getPendingPromise();
+    session.on('Animation.animationStarted', payload => {
+      expect(payload.animation.type).toBe('CSSAnimation');
+      resolve();
+    });
+    await page.goto(`http://localhost:${appPort}/start`);
+    await end;
+  });
+});
