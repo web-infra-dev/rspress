d33b10060983843ade77633dcb277b11079e2b74
zoolsher <zoolsher@gmail.com>
2023-09-27 10:18:37 +0800

    chore: add test case for non-suffix

---
+++ e2e/fixtures/nav-link-items-without-suffix/rspress.config.ts
@@ -0,0 +1,9 @@
+import * as path from 'path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'doc'),
+  route: {
+    cleanUrls: true,
+  },
+});
