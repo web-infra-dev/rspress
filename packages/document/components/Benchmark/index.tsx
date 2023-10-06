13aba4f48b5d20ca494aea1b0696eea00187c7de
yangxingyuan <yangxingyuan@bytedance.com>
2023-10-06 20:55:17 +0800

    chore: remove useless dep

--- packages/document/components/Benchmark/index.tsx
+++ packages/document/components/Benchmark/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
 import { Tabs, Tab } from 'rspress/theme';
 import { useI18n } from 'rspress/runtime';
 import { ProgressBar } from './ProgressBar';
@@ -56,7 +55,6 @@ export function Benchmark() {
   const t = useI18n();
   const [activeScene, setActiveScene] =
     useState<keyof typeof BENCHMARK_DATA>('start');
-  const { ref, inView } = useInView();
   const variants = {
     initial: { y: 50, opacity: 0 },
     animate: { y: 0, opacity: 1, transition: { duration: 0.5 } },
@@ -64,14 +62,13 @@ export function Benchmark() {
   const performanceInfoList = BENCHMARK_DATA[activeScene];
   return (
     <motion.div
-      ref={ref}
       initial={{ opacity: 0, y: 50 }}
-      animate={inView ? 'animate' : 'initial'}
+      animate={'animate'}
       variants={variants}
       transition={{ duration: 1 }}
       className="relative flex flex-col justify-center py-10 mt-15 h-auto"
     >
-      {inView && (
+      {
         <>
           <div
             className="flex flex-col items-center z-1"
@@ -79,7 +76,6 @@ export function Benchmark() {
               padding: '16px 0',
             }}
           >
-            {/* <h2 className="font-bold text-2xl mb-5">超快的编译速度!</h2> */}
             <Tabs
               values={SCENE.map(item => ({
                 label: t(item as keyof typeof BENCHMARK_DATA),
@@ -98,7 +94,7 @@ export function Benchmark() {
                         margin: '16px 16px 16px 0',
                       }}
                     >
-                      {inView && (
+                      {
                         <>
                           <p
                             className="mr-2 mb-2 w-20 text-center text-gray-500 dark:text-light-500"
@@ -113,7 +109,7 @@ export function Benchmark() {
                             )}
                           />
                         </>
-                      )}
+                      }
                     </div>
                   ))}
                 </Tab>
@@ -121,7 +117,7 @@ export function Benchmark() {
             </Tabs>
           </div>
         </>
-      )}
+      }
     </motion.div>
   );
 }
