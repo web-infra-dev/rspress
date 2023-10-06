4427bc6de803def430ca290c81474288164a573b
yangxingyuan <yangxingyuan@bytedance.com>
2023-10-06 18:20:01 +0800

    chore: add predefined question

--- packages/document/theme/index.tsx
+++ packages/document/theme/index.tsx
@@ -1,18 +1,39 @@
 import Theme from 'rspress/theme';
-import { NoSSR } from 'rspress/runtime';
+import { NoSSR, useLang } from 'rspress/runtime';
 import { Documate } from '@documate/react';
 import '@documate/react/dist/style.css';
 import './index.css';
 
-const Layout = () => (
-  <Theme.Layout
-    afterNavTitle={
-      <NoSSR>
-        <Documate endpoint={`${process.env.DOCUMATE_BACKEND_URL}/ask`} />
-      </NoSSR>
-    }
-  />
-);
+const predefinedQuestions = {
+  zh: [
+    '什么是 Rspress？',
+    '在 Rspress 中如何自定义全局样式？',
+    '提供一个简单的 Rspress 插件示例。',
+    '如何在 Rspress 中自定义主题？',
+  ],
+  en: [
+    'What is Rspress?',
+    'How to customize global styles in Rspress?',
+    'Provide a simple Rspress plugin example。',
+    'How to customize themes in Rspress?',
+  ],
+};
+
+const Layout = () => {
+  const lang = useLang();
+  return (
+    <Theme.Layout
+      afterNavTitle={
+        <NoSSR>
+          <Documate
+            endpoint={`${process.env.DOCUMATE_BACKEND_URL}/ask`}
+            predefinedQuestions={predefinedQuestions[lang as 'zh' | 'en']}
+          />
+        </NoSSR>
+      }
+    />
+  );
+};
 
 export default {
   ...Theme,
