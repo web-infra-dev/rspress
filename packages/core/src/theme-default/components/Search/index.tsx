67327164594fca342caa6a02c175cb95db10b48d
Riri <Daydreamerriri@outlook.com>
2023-10-12 15:31:52 +0800

    feat: set metakey `ctrl` in windows (#173)

--- packages/core/src/theme-default/components/Search/index.tsx
+++ packages/core/src/theme-default/components/Search/index.tsx
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchSvg from './assets/search.svg';
 import styles from './index.module.scss';
 import { SearchPanel } from './SearchPanel';
 
 export function Search() {
   const [focused, setFocused] = useState(false);
+  const [metaKey, setMetaKey] = useState<null | string>(null);
+
+  useEffect(() => {
+    setMetaKey(
+      /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform) ? '⌘' : 'Ctrl',
+    );
+  }, []);
+
   return (
     <>
       <div className={styles.navSearchButton} onClick={() => setFocused(true)}>
         <button>
           <SearchSvg width="18" hight="18" />
           <p className={styles.searchWord}>Search Docs</p>
-          <div>
-            <span>⌘</span>
+          <div style={{ opacity: metaKey ? 1 : 0 }}>
+            <span>{metaKey}</span>
             <span>K</span>
           </div>
         </button>
