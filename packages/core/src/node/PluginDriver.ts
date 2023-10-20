1d0816edeb7085daa0d246e23a2a83b87b319edc
yangxingyuan <yangxingyuan@bytedance.com>
2023-10-20 16:49:49 +0800

    feat: add runtime modules hook

--- packages/core/src/node/PluginDriver.ts
+++ packages/core/src/node/PluginDriver.ts
@@ -124,11 +124,15 @@ export class PluginDriver {
   async modifySearchIndexData(
     pages: PageIndexInfo[],
   ): Promise<PageIndexInfo[]> {
-    return this._runParallelAsyncHook('modifySearchIndexData', pages);
+    return this._runParallelAsyncHook(
+      'modifySearchIndexData',
+      pages,
+      this.#isProd,
+    );
   }
 
   async extendPageData(pageData: PageIndexInfo) {
-    return this._runParallelAsyncHook('extendPageData', pageData);
+    return this._runParallelAsyncHook('extendPageData', pageData, this.#isProd);
   }
 
   async addPages() {
@@ -143,6 +147,21 @@ export class PluginDriver {
 
   async routeGenerated(routes: RouteMeta[]) {
     return this._runParallelAsyncHook('routeGenerated', routes);
+  }
+
+  async addRuntimeModules() {
+    const result: Record<string, string>[] = await this._runParallelAsyncHook(
+      'addRuntimeModules',
+      this.#config || {},
+      this.#isProd,
+    );
+
+    return result.reduce((prev, current) => {
+      return {
+        ...prev,
+        ...current,
+      };
+    }, {});
   }
 
   async addSSGRoutes() {
