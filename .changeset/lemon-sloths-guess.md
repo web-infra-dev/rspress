0f8179921b0adf64cec290f812ead57c694b870c
yangxingyuan <yangxingyuan@bytedance.com>
2023-11-12 09:12:03 +0800

    feat: support render html in feature detail

---
+++ .changeset/lemon-sloths-guess.md
@@ -0,0 +1,5 @@
+---
+'@rspress/theme-default': patch
+---
+
+feat: support render html in feature card details
--- packages/theme-default/src/components/HomeFeatures/index.tsx
+++ packages/theme-default/src/components/HomeFeatures/index.tsx
@@ -1,5 +1,6 @@
 import { FrontMatterMeta, Feature } from '@rspress/shared';
 import styles from './index.module.scss';
+import { renderHtmlOrText } from '@/logic';
 
 const GRID_PREFIX = 'grid-';
 
@@ -25,7 +26,7 @@ export function HomeFeature({ frontmatter }: { frontmatter: FrontMatterMeta }) {
             <div className="h-full p-2">
               <article
                 key={title}
-                className={`${styles.featureCard} h-full p-8 rounded-4xl border-transparent`}
+                className={`rspress-home-feature-card ${styles.featureCard} h-full p-8 rounded-4xl border-transparent`}
                 style={{
                   cursor: link ? 'pointer' : 'auto',
                 }}
@@ -35,16 +36,19 @@ export function HomeFeature({ frontmatter }: { frontmatter: FrontMatterMeta }) {
                   }
                 }}
               >
-                <div className="flex-center">
-                  <div className="rspress-home-feature-icon w-12 h-12 text-3xl text-center">
-                    {icon}
+                {icon ? (
+                  <div className="flex-center">
+                    <div className="rspress-home-feature-icon w-12 h-12 text-3xl text-center">
+                      {icon}
+                    </div>
                   </div>
-                </div>
+                ) : null}
+
                 <h2 className="rspress-home-feature-title font-bold text-center">
                   {title}
                 </h2>
                 <p className="rspress-home-feature-detail leading-6 pt-2 text-sm text-text-2 font-medium">
-                  {details}
+                  {renderHtmlOrText(details)}
                 </p>
               </article>
             </div>
