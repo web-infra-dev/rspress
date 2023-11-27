1cb0fe50879a4f2cc7c16b622c563c68286666d5
sumy <sumyggsun@gmail.com>
2023-11-28 02:07:20 +0800

    fix: devServer use searchIndexMiddleware first.

--- packages/core/src/node/initRsbuild.ts
+++ packages/core/src/node/initRsbuild.ts
@@ -96,10 +96,10 @@ async function createInternalBuildConfig(
       setupMiddlewares: [
         middlewares => {
           if (isPublicDirExist) {
-            middlewares.push(sirv(publicDir));
+            middlewares.unshift(sirv(publicDir));
           }
 
-          middlewares.push(serveSearchIndexMiddleware(config));
+          middlewares.unshift(serveSearchIndexMiddleware(config));
         },
       ],
     },
