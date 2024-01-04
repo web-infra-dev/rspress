8239618c6a7548d3feae18434d3e5cf22143e2ba
yangxingyuan <39261479+sanyuan0704@users.noreply.github.com>
2024-01-04 15:29:02 +0800

    fix: custom tsx file error (#523)

--- packages/core/src/node/runtimeModule/siteData/extractPageData.ts
+++ packages/core/src/node/runtimeModule/siteData/extractPageData.ts
@@ -22,117 +22,121 @@ export async function extractPageData(
   routeService: RouteService,
 ): Promise<(PageIndexInfo | null)[]> {
   return Promise.all(
-    routeService
-      .getRoutes()
-      .filter(route => MDX_REGEXP.test(route.absolutePath))
-      .map(async (route, index) => {
-        let content: string = await fs.readFile(route.absolutePath, 'utf8');
-        const { frontmatter, content: strippedFrontMatter } = loadFrontMatter(
-          content,
-          route.absolutePath,
-          root,
-        );
+    routeService.getRoutes().map(async (route, index) => {
+      const defaultIndexInfo: PageIndexInfo = {
+        id: index,
+        title: '',
+        content: '',
+        routePath: route.routePath,
+        lang: route.lang,
+        toc: [],
+        domain,
+        frontmatter: {},
+        version: route.version,
+        _filepath: route.absolutePath,
+        _relativePath: path.relative(root, route.absolutePath),
+      };
+      if (!MDX_REGEXP.test(route.absolutePath)) {
+        return defaultIndexInfo;
+      }
+      let content: string = await fs.readFile(route.absolutePath, 'utf8');
+      const { frontmatter, content: strippedFrontMatter } = loadFrontMatter(
+        content,
+        route.absolutePath,
+        root,
+      );
 
-        // 1. Replace rules for frontmatter & content
-        Object.keys(frontmatter).forEach(key => {
-          if (typeof frontmatter[key] === 'string') {
-            frontmatter[key] = applyReplaceRules(
-              frontmatter[key],
-              replaceRules,
-            );
-          }
-        });
-
-        // TODO: we will find a more efficient way to do this
-        const flattenContent = await flattenMdxContent(
-          applyReplaceRules(strippedFrontMatter, replaceRules),
-          route.absolutePath,
-          alias,
-        );
-
-        content = flattenContent.replace(importStatementRegex, '');
-
-        const {
-          html,
-          title,
-          toc: rawToc,
-        } = await compile({
-          value: content,
-          filepath: route.absolutePath,
-          development: process.env.NODE_ENV !== 'production',
-          root,
-        });
-
-        if (!title?.length && !frontmatter && !frontmatter.title?.length) {
-          return null;
+      // 1. Replace rules for frontmatter & content
+      Object.keys(frontmatter).forEach(key => {
+        if (typeof frontmatter[key] === 'string') {
+          frontmatter[key] = applyReplaceRules(frontmatter[key], replaceRules);
         }
-        content = htmlToText(String(html), {
-          wordwrap: 80,
-          selectors: [
-            {
-              selector: 'a',
-              options: {
-                ignoreHref: true,
-              },
+      });
+
+      // TODO: we will find a more efficient way to do this
+      const flattenContent = await flattenMdxContent(
+        applyReplaceRules(strippedFrontMatter, replaceRules),
+        route.absolutePath,
+        alias,
+      );
+
+      content = flattenContent.replace(importStatementRegex, '');
+
+      const {
+        html,
+        title,
+        toc: rawToc,
+      } = await compile({
+        value: content,
+        filepath: route.absolutePath,
+        development: process.env.NODE_ENV !== 'production',
+        root,
+      });
+
+      if (!title?.length && !frontmatter && !frontmatter.title?.length) {
+        return null;
+      }
+      content = htmlToText(String(html), {
+        wordwrap: 80,
+        selectors: [
+          {
+            selector: 'a',
+            options: {
+              ignoreHref: true,
             },
-            {
-              selector: 'img',
-              format: 'skip',
-            },
-            {
-              // Skip code blocks
-              selector: 'pre > code',
-              format: 'skip',
-            },
-            ...['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map(tag => ({
-              selector: tag,
-              options: {
-                uppercase: false,
-              },
-            })),
-          ],
-          tables: true,
-          longWordSplit: {
-            forceWrapOnLimit: true,
           },
-        });
-        if (content.startsWith(title)) {
-          // Remove the title from the content
-          content = content.slice(title.length);
-        }
+          {
+            selector: 'img',
+            format: 'skip',
+          },
+          {
+            // Skip code blocks
+            selector: 'pre > code',
+            format: 'skip',
+          },
+          ...['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map(tag => ({
+            selector: tag,
+            options: {
+              uppercase: false,
+            },
+          })),
+        ],
+        tables: true,
+        longWordSplit: {
+          forceWrapOnLimit: true,
+        },
+      });
+      if (content.startsWith(title)) {
+        // Remove the title from the content
+        content = content.slice(title.length);
+      }
 
-        const toc: Header[] = rawToc.map(item => {
-          // If the item.id ends with '-number', we take the number
-          const match = item.id.match(/-(\d+)$/);
-          let position = -1;
-          if (match) {
-            for (let i = 0; i < Number(match[1]); i++) {
-              position = content.indexOf(`\n${item.text}#\n\n`, position + 1);
-            }
+      const toc: Header[] = rawToc.map(item => {
+        // If the item.id ends with '-number', we take the number
+        const match = item.id.match(/-(\d+)$/);
+        let position = -1;
+        if (match) {
+          for (let i = 0; i < Number(match[1]); i++) {
+            position = content.indexOf(`\n${item.text}#\n\n`, position + 1);
           }
-          return {
-            ...item,
-            charIndex: content.indexOf(`\n${item.text}#\n\n`, position + 1),
-          };
-        });
-
+        }
         return {
-          id: index,
-          title: frontmatter.title || title,
-          routePath: route.routePath,
-          lang: route.lang,
-          toc,
-          domain,
-          // Stripped frontmatter content
-          content,
-          frontmatter: {
-            ...frontmatter,
-            __content: undefined,
-          },
-          version: route.version,
-          _filepath: route.absolutePath,
-          _relativePath: path.relative(root, route.absolutePath),
+          ...item,
+          charIndex: content.indexOf(`\n${item.text}#\n\n`, position + 1),
         };
-      }),
+      });
+
+      return {
+        ...defaultIndexInfo,
+        title: frontmatter.title || title,
+        toc,
+        // Stripped frontmatter content
+        content,
+        frontmatter: {
+          ...frontmatter,
+          __content: undefined,
+        },
+      };
+    }),
   );
 }
