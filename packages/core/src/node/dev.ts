5cf2f7ef1a1bc422b5a31ff9c6a07cb724129d22
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2024-01-12 12:15:07 +0800

    chore(deps): bump rsbuild to 0.3.1

--- packages/core/src/node/dev.ts
+++ packages/core/src/node/dev.ts
@@ -1,5 +1,5 @@
-import { UserConfig, removeLeadingSlash } from '@rspress/shared';
-import type { RsbuildConfig } from '@rsbuild/core';
+import { UserConfig } from '@rspress/shared';
+import { type RsbuildConfig, logger as rsbuildLogger } from '@rsbuild/core';
 import { logger } from '@rspress/shared/logger';
 import { initRsbuild } from './initRsbuild';
 import { writeSearchIndex } from './searchIndex';
@@ -18,7 +18,6 @@ interface DevOptions {
 
 export async function dev(options: DevOptions): Promise<ServerInstance> {
   const { docDirectory, config, extraBuilderConfig } = options;
-  const base = config?.base ?? '';
   const isProd = false;
   const pluginDriver = new PluginDriver(config, isProd);
   await pluginDriver.init();
@@ -26,6 +25,9 @@ export async function dev(options: DevOptions): Promise<ServerInstance> {
   try {
     const modifiedConfig = await pluginDriver.modifyConfig();
     await pluginDriver.beforeBuild();
+
+    rsbuildLogger.override(logger);
+
     // empty temp dir before build
     // await fs.emptyDir( TEMP_DIR);
     const builder = await initRsbuild(
@@ -36,15 +38,8 @@ export async function dev(options: DevOptions): Promise<ServerInstance> {
       extraBuilderConfig,
     );
     const { server } = await builder.startDevServer({
-      printURLs: urls => {
-        return urls.map(({ label, url }) => ({
-          label,
-          url: `${url}/${removeLeadingSlash(base)}`,
-        }));
-      },
       // We will support the following options in the future
       getPortSilently: true,
-      logger,
     });
 
     await pluginDriver.afterBuild();
