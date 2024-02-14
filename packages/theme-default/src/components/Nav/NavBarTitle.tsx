5c69c0ba5b6bbe0359cbd2f9e25c3564724955f7
Arnav K <arnavkaushal09@gmail.com>
2024-02-14 14:07:14 +0530

    Update NavBarTitle.tsx

--- packages/theme-default/src/components/Nav/NavBarTitle.tsx
+++ packages/theme-default/src/components/Nav/NavBarTitle.tsx
@@ -33,6 +33,7 @@ export const NavBarTitle = () => {
             id="logo"
             className="mr-4 rspress-logo"
             height={26}
+            width="auto"
           />
         ) : (
           <span>{title}</span>
