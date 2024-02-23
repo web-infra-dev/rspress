0ab760679f5b05a9024a2a7a9fee95b7e6192c7e
yangxingyuan <yangxingyuan@bytedance.com>
2024-02-23 11:11:49 +0800

    fix: adjust public dir

--- packages/document/rspress.config.ts
+++ packages/document/rspress.config.ts
@@ -15,12 +15,6 @@ export default defineConfig({
   },
   icon: 'https://lf3-static.bytednsdoc.com/obj/eden-cn/uhbfnupenuhf/rspress/rspress-logo.png',
   builderConfig: {
-    server: {
-      publicDir: {
-        name: 'public',
-        copyOnBuild: true,
-      },
-    },
     dev: {
       startUrl: false,
     },
