f426d2fb1bc03818c0c93379d47632114f20da18
Arnav K <arnavkaushal09@gmail.com>
2024-02-28 12:12:40 +0530

    feat(internal-components): New Steps Component! (#672)

---
+++ .changeset/chilled-moons-listen.md
@@ -0,0 +1,7 @@
+---
+"@rspress/theme-default": minor
+"@rspress/docs": patch
+---
+
+feat(internal-components): New Steps Component!
+patch(docs): Add documentation for Steps Component!
---
+++ packages/theme-default/src/components/Steps/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import styles from './index.module.scss';
+
+export function Steps({ children }: { children: ReactNode }) {
+  return (
+    <div
+      className={`ml-4 mb-11 border-l pl-6 ${styles.rspressSteps} [counter-reset:step]`}
+    >
+      {children}
+    </div>
+  );
+}
