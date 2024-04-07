63107dfc858bb6358965d5f6cbbba2f8c5a1d938
yukkrui <37723532+seeintea@users.noreply.github.com>
2024-04-07 14:11:34 +0800

    fix(theme): homepage mask based on logo vertical centering (#917)

--- packages/theme-default/src/components/HomeHero/index.tsx
+++ packages/theme-default/src/components/HomeHero/index.tsx
@@ -26,7 +26,7 @@ export function HomeHero({ frontmatter }: { frontmatter: FrontMatterMeta }) {
         .filter(text => text !== '')
     : [];
   return (
-    <div className="m-auto pt-0 px-6 pb-12 sm:pt-10 sm:px-16 md:pt-16 md:px-16 md:pb-16">
+    <div className="m-auto pt-0 px-6 pb-12 sm:pt-10 sm:px-16 md:pt-16 md:px-16 md:pb-16 relative">
       <div
         className={styles.mask}
         style={{
