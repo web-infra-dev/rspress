62e9a387bb90eab2c29f023407281570ea734e4e
sumy <sumyggsun@gmail.com>
2024-04-11 08:58:40 +0800

    feat: no need check url extension.

--- packages/shared/src/runtime-utils/index.ts
+++ packages/shared/src/runtime-utils/index.ts
@@ -242,9 +242,7 @@ export function normalizeHref(url?: string, cleanUrls = false) {
   // eslint-disable-next-line prefer-const
   let { url: cleanUrl, hash } = parseUrl(decodeURIComponent(url));
 
-  const hasHtmlExt = cleanUrl.endsWith('.html');
-
-  if (!cleanUrls && !cleanUrl.endsWith('.html') && !hasHtmlExt) {
+  if (!cleanUrls && !cleanUrl.endsWith('.html')) {
     if (cleanUrl.endsWith('/')) {
       cleanUrl += 'index.html';
     } else {
