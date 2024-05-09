a7c7393554fe5f3ed2e25142e207c697cf51b5d1
dalaoshu <laipichan@qq.com>
2024-05-10 02:07:24 +0800

    test(core): add unit test for prismLanguages

---
+++ packages/core/tests/prismLanguages.test.ts
@@ -0,0 +1,36 @@
+import path from 'path';
+import { describe, expect, it } from 'vitest';
+import { RuntimeModuleID } from '../src/node/runtimeModule';
+import { siteDataVMPlugin } from '../src/node/runtimeModule/siteData';
+
+describe('automatic import of prism languages', () => {
+  const userDocRoot = path.join(__dirname, 'mdx');
+
+  const context: any = {
+    alias: {},
+    config: {
+      markdown: {
+        highlightLanguages: [['js', 'javascript']],
+      },
+    },
+    userDocRoot,
+    routeService: {
+      getRoutes() {
+        return [
+          { absolutePath: path.join(userDocRoot, 'prismLanguages.test.mdx') },
+        ];
+      },
+    },
+    pluginDriver: {
+      async extendPageData(_pageData: any) {},
+      async modifySearchIndexData(_pages: any) {},
+    },
+  };
+
+  it('prism languages aliases should be configurable to users', async () => {
+    const meta = await siteDataVMPlugin(context);
+    const prismLanguagesContent = meta[RuntimeModuleID.PrismLanguages];
+
+    expect(prismLanguagesContent).toMatchSnapshot();
+  });
+});
