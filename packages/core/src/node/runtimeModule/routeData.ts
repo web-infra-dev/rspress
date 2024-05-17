378f582b748ac465770c5c8246dec16a922cc6b1
Timeless0911 <1604889533@qq.com>
2024-05-17 13:54:40 +0800

    fix: not generate ssr route runtime module when client build

--- packages/core/src/node/runtimeModule/routeData.ts
+++ packages/core/src/node/runtimeModule/routeData.ts
@@ -1,8 +1,15 @@
 import { FactoryContext, RuntimeModuleID } from '.';
 
 export async function routeVMPlugin(context: FactoryContext) {
-  const { routeService } = context;
+  const { routeService, isSSR } = context;
+
   // client: The components of route is lazy loaded
+  if (!isSSR) {
+    return {
+      [RuntimeModuleID.RouteForClient]: routeService.generateRoutesCode(false),
+    };
+  }
+
   return {
     [RuntimeModuleID.RouteForClient]: routeService.generateRoutesCode(false),
     [RuntimeModuleID.RouteForSSR]: routeService.generateRoutesCode(true),
