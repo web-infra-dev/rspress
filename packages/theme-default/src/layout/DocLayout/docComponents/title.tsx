99c202f49e4ad46eafd3e2f5e868242cb45f7b76
neverland <chenjiahan.jait@bytedance.com>
2024-06-01 13:32:03 +0800

    feat: increase title font size to distinguish from main text (#1136)

--- packages/theme-default/src/layout/DocLayout/docComponents/title.tsx
+++ packages/theme-default/src/layout/DocLayout/docComponents/title.tsx
@@ -26,9 +26,7 @@ export const H3 = (props: React.ComponentProps<'h3'>) => {
 };
 
 export const H4 = (props: React.ComponentProps<'h4'>) => {
-  return (
-    <h4 {...props} className={`mt-8 leading-6 text-base ${styles.title}`} />
-  );
+  return <h4 {...props} className={`mt-8 leading-6 text-lg ${styles.title}`} />;
 };
 
 export const H5 = (props: React.ComponentProps<'h5'>) => {
