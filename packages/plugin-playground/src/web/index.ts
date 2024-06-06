90fc852c04a63e48d1cf2e489dcf5d03b7c2bfec
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2024-06-06 13:45:05 +0800

    fix(plugin-playground): export EditorProps error (#1157)

--- packages/plugin-playground/src/web/index.ts
+++ packages/plugin-playground/src/web/index.ts
@@ -1,7 +1,7 @@
 export {
   default as MonacoEditor,
   loader as MonacoEditorLoader,
-  EditorProps as MonacoEditorProps,
+  type EditorProps as MonacoEditorProps,
 } from '@monaco-editor/react';
 export { Editor } from './editor';
 export { Runner } from './runner';
