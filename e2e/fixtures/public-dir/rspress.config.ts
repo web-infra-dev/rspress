392810a62fe9773b3e997825117a0d08f6381600
SoonIter <sooniter@gmail.com>
2024-08-02 16:02:11 +0800

    chore: update

--- e2e/fixtures/public-dir/rspress.config.ts
+++ e2e/fixtures/public-dir/rspress.config.ts
@@ -3,15 +3,4 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'doc'),
-  globalStyles: path.join(__dirname, 'styles/index.css'),
-  themeConfig: {
-    socialLinks: [
-      {
-        icon: 'github',
-        mode: 'dom',
-        content:
-          '<a href="/zh" class="flex items-center text-base font-semibold transition-opacity duration-300 hover:opacity-60" style="width: max-content"><img src="https://lf3-static.bytednsdoc.com/obj/eden-cn/rjhwzy/ljhwZthlaukjlkulzlp/rspress/rspress-navbar-logo-0904.png" alt="logo" id="logo" class="mr-4 rspress-logo dark:hidden" style=" height: 90px; width: 360px"><img src="https://lf3-static.bytednsdoc.com/obj/eden-cn/rjhwzy/ljhwZthlaukjlkulzlp/rspress/rspress-navbar-logo-dark-0904.png" alt="logo" id="logo" class="mr-4 rspress-logo hidden dark:block" style=" height: 45px; width: 180px"></a>',
-      },
-    ],
-  },
 });
