aeaa7216665de74574c09c33e9e1c1c74caf4de0
Timeless0911 <1604889533@qq.com>
2024-08-05 19:36:05 +0800

    docs: bump `@rstack-dev/doc-ui` to add Rslib and awesome Rspack

--- packages/document/theme/index.tsx
+++ packages/document/theme/index.tsx
@@ -1,5 +1,5 @@
 import Theme from 'rspress/theme';
-import { NavIcon } from 'rsfamily-doc-ui/nav-icon';
+import { NavIcon } from '@rstack-dev/doc-ui/nav-icon';
 import { HomeLayout as BasicHomeLayout } from 'rspress/theme';
 import { ToolStack } from './components/ToolStack';
 import './index.css';
