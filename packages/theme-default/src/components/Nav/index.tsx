b3624ade9326f273e6d17e97e59befd180b5155f
chenjiahan <chenjiahan.jait@bytedance.com>
2024-09-12 22:42:29 +0800

    feat(style): use a compact layout when there are many nav items

--- packages/theme-default/src/components/Nav/index.tsx
+++ packages/theme-default/src/components/Nav/index.tsx
@@ -69,6 +69,7 @@ export function Nav(props: NavProps) {
               langs={langs}
               base={base}
               key={item.link}
+              shrink={menuItems.length > 5}
               {...item}
             />
           );
