f550732f9fd8395aad2b2fe53f4cb8e5f85790d3
Arnav K <arnavkaushal09@gmail.com>
2024-10-17 16:30:49 +0000

    f

--- e2e/fixtures/github-alert/rspress.config.ts
+++ e2e/fixtures/github-alert/rspress.config.ts
@@ -6,14 +6,4 @@ export default defineConfig({
   markdown: {
     mdxRs: false,
   },
-  themeConfig: {
-    socialLinks: [
-      {
-        icon: 'github',
-        mode: 'dom',
-        content:
-          '<a href="/zh" class="flex items-center text-base font-semibold transition-opacity duration-300 hover:opacity-60" style="width: max-content"><img src="https://lf3-static.bytednsdoc.com/obj/eden-cn/rjhwzy/ljhwZthlaukjlkulzlp/rspress/rspress-navbar-logo-0904.png" alt="logo" id="logo" class="mr-4 rspress-logo dark:hidden" style=" height: 90px; width: 360px"><img src="https://lf3-static.bytednsdoc.com/obj/eden-cn/rjhwzy/ljhwZthlaukjlkulzlp/rspress/rspress-navbar-logo-dark-0904.png" alt="logo" id="logo" class="mr-4 rspress-logo hidden dark:block" style=" height: 45px; width: 180px"></a>',
-      },
-    ],
-  },
 });
