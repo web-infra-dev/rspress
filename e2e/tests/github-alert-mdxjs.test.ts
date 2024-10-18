4ee5a8d23303627cde708461a16ae431f3314e14
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2024-10-18 15:48:51 +0800

    test: add github alert syntax of mdx-js tests (#1502)

---
+++ e2e/tests/github-alert-mdxjs.test.ts
@@ -0,0 +1,49 @@
+import { expect, test } from '@playwright/test';
+import path from 'node:path';
+import { getPort, killProcess, runDevCommand } from '../utils/runCommands';
+import { getNavbar, getSidebar } from '../utils/getSideBar';
+
+const fixtureDir = path.resolve(__dirname, '../fixtures');
+
+test.describe('github alert syntax in mdx-js', async () => {
+  let appPort;
+  let app;
+  test.beforeAll(async () => {
+    const appDir = path.join(fixtureDir, 'github-alert-mdxjs');
+    appPort = await getPort();
+    app = await runDevCommand(appDir, appPort);
+  });
+
+  test.afterAll(async () => {
+    if (app) {
+      await killProcess(app);
+    }
+  });
+
+  test('should transform github alert syntax to contain syntax', async ({
+    page,
+  }) => {
+    await page.goto(`http://localhost:${appPort}`);
+
+    const directives = await page.$$(
+      '.rspress-doc > [class^="rspress-directive"]',
+    );
+    expect(directives.length).toEqual(7);
+
+    const containerTypes = await Promise.all(
+      directives.map(async directive => {
+        const className = await directive.getAttribute('class');
+        return className?.split(' ')[1];
+      }),
+    );
+    expect(containerTypes).toEqual([
+      'tip',
+      'note',
+      'warning',
+      'caution',
+      'danger',
+      'info',
+      'details',
+    ]);
+  });
+});
