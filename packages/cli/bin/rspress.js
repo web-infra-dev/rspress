795c883871a9ca918b3c58b0d4d26c512ceedd08
neverland <chenjiahan.jait@bytedance.com>
2024-11-04 16:27:41 +0800

    perf(cli): enable Node.js compile cache (#1549)

--- packages/cli/bin/rspress.js
+++ packages/cli/bin/rspress.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
-// eslint-disable-next-line import/no-useless-path-segments, node/file-extension-in-import
+import nodeModule from 'node:module';
+
+// enable on-disk code caching of all modules loaded by Node.js
+// requires Nodejs >= 22.8.0
+const { enableCompileCache } = nodeModule;
+if (enableCompileCache) {
+  try {
+    enableCompileCache();
+  } catch {
+    // ignore errors
+  }
+}
+
 import('../dist/index.js');
