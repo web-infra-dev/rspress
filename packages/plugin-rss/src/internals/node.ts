cd5db6a24e3ab32e0626ba977a1ba6aa7823065e
neverland <chenjiahan.jait@bytedance.com>
2024-11-16 19:38:25 +0800

    chore(deps): bump pnpm from v8 to v9

--- packages/plugin-rss/src/internals/node.ts
+++ packages/plugin-rss/src/internals/node.ts
@@ -1,8 +1,11 @@
-import { mkdir, writeFile as _writeFile } from 'node:fs/promises';
+import { promises } from 'node:fs';
 import * as NodePath from 'node:path';
 
-export async function writeFile(path: string, content: string | Buffer) {
+export async function writeFile(
+  path: string,
+  content: Parameters<typeof promises.writeFile>[1],
+) {
   const dir = NodePath.dirname(path);
-  await mkdir(dir, { mode: 0o755, recursive: true });
-  return _writeFile(path, content);
+  await promises.mkdir(dir, { recursive: true });
+  return promises.writeFile(path, content);
 }
