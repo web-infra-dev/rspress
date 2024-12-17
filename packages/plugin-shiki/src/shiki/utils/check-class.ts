118d17eac637681e7943410433252912d28dfd09
JounQin <admin@1stg.me>
2024-12-17 14:12:15 +0800

    feat: bump shiki to v1, migrate to official transformers

--- packages/plugin-shiki/src/shiki/utils/check-class.ts
+++
@@ -1,5 +0,0 @@
-export const checkClass = (code: string, className: string | string[]) => {
-  const classes = Array.isArray(className) ? className.join('') : className;
-
-  return code.search(classes) !== -1;
-};
