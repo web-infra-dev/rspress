7ca5c8d7efa5a5266d2a1d4bdde6b8e9a53696a3
SoonIter <sooniter@gmail.com>
2025-02-19 14:19:43 +0800

    perf: isActive performance

--- packages/runtime/src/route.ts
+++ packages/runtime/src/route.ts
@@ -9,15 +9,24 @@ function normalizeRoutePath(routePath: string) {
     .replace(/\/index$/, '/');
 }
 
+const cache = new Map<string, Route>();
 /**
  * this is a bridge of two core features Sidebar and RouteService
  * @param pathname useLocation().pathname
  * @returns
  */
 export function pathnameToRouteService(pathname: string): Route | undefined {
+  const cacheItem = cache.get(pathname);
+  if (cacheItem) {
+    return cacheItem;
+  }
   const matched = matchRoutes(
     routes as typeof import('virtual-routes')['routes'],
     normalizeRoutePath(pathname),
   );
-  return matched?.[0]?.route;
+  const route: Route | undefined = matched?.[0]?.route;
+  if (route) {
+    cache.set(pathname, route);
+  }
+  return route;
 }
