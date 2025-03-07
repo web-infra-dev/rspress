26ae63d9b75d8222f94c44fa6b173acf1c58e108
JounQin <admin@1stg.me>
2025-03-07 15:51:11 +0800

    fix: display outline correctly on >=960px <=1280px screen width (#1918)

--- packages/theme-default/src/components/LocalSideBar/index.tsx
+++ packages/theme-default/src/components/LocalSideBar/index.tsx
@@ -1,41 +1,41 @@
 import { useLocation } from '@rspress/runtime';
-import { Sidebar, Toc } from '@theme';
+import { Toc } from '@theme';
 import ArrowRight from '@theme-assets/arrow-right';
 import MenuIcon from '@theme-assets/menu';
-import { Fragment, useEffect, useRef, useState } from 'react';
-import './index.scss';
+import { useEffect, useRef, useState } from 'react';
 import type { UISwitchResult } from '../../logic/useUISwitch';
 import { SvgWrapper } from '../SvgWrapper';
+import './index.scss';
 
-export function SideMenu({
+/* Top Menu, only displayed on <1280px screen width */
+export const SidebarMenu = ({
+  isSidebarOpen,
+  onIsSidebarOpenChange,
   outlineTitle,
-  beforeSidebar,
-  afterSidebar,
   uiSwitch,
-  navTitle,
 }: {
+  isSidebarOpen: boolean;
+  onIsSidebarOpenChange: (isOpen: boolean) => void;
   outlineTitle: string;
-  beforeSidebar?: React.ReactNode;
-  afterSidebar?: React.ReactNode;
   uiSwitch?: UISwitchResult;
-  navTitle?: React.ReactNode;
-}) {
-  const [isSidebarOpen, setSidebarIsOpen] = useState<boolean>(false);
-  const [isTocOpen, setIsTocOpen] = useState<boolean>(false);
+}) => {
   const tocContainerRef = useRef<HTMLDivElement>(null);
   const outlineButtonRef = useRef<HTMLButtonElement>(null);
+
+  const [isTocOpen, setIsTocOpen] = useState<boolean>(false);
+
   const { pathname } = useLocation();
 
   function openSidebar() {
-    setSidebarIsOpen(true);
+    onIsSidebarOpenChange(true);
   }
 
   function closeSidebar() {
-    setSidebarIsOpen(false);
+    onIsSidebarOpenChange(false);
   }
 
   useEffect(() => {
-    setSidebarIsOpen(false);
+    onIsSidebarOpenChange(false);
   }, [pathname]);
 
   useEffect(() => {
@@ -61,60 +61,16 @@ export function SideMenu({
   };
 
   return (
-    <Fragment>
-      {/* Top Menu, only displayed in mobile device */}
-      <div className="rspress-sidebar-menu">
-        {uiSwitch?.showSidebar ? (
+    <div className="rspress-sidebar-menu">
+      {uiSwitch?.showSidebar && (
+        <>
           <button onClick={openSidebar} className="flex-center mr-auto">
             <div className="text-md mr-2">
               <SvgWrapper icon={MenuIcon} />
             </div>
             <span className="text-sm">Menu</span>
           </button>
-        ) : null}
-        {uiSwitch?.showAside ? (
-          <Fragment>
-            <button
-              onClick={() => setIsTocOpen(tocOpened => !tocOpened)}
-              className="flex-center ml-auto"
-              ref={outlineButtonRef}
-            >
-              <span className="text-sm">{outlineTitle}</span>
-              <div
-                className="text-md mr-2"
-                style={{
-                  transform: isTocOpen ? 'rotate(90deg)' : 'rotate(0deg)',
-                  transition: 'transform 0.2s ease-out',
-                  marginTop: '2px',
-                }}
-              >
-                <SvgWrapper icon={ArrowRight} />
-              </div>
-            </button>
-
-            <div
-              className={`rspress-local-toc-container ${isTocOpen ? 'rspress-local-toc-container-show' : ''}`}
-            >
-              <Toc
-                onItemClick={() => {
-                  setIsTocOpen(false);
-                }}
-              />
-            </div>
-          </Fragment>
-        ) : null}
-      </div>
-      {/* Sidebar Component */}
-      {uiSwitch?.showSidebar ? (
-        <Fragment>
-          <Sidebar
-            isSidebarOpen={isSidebarOpen}
-            beforeSidebar={beforeSidebar}
-            afterSidebar={afterSidebar}
-            uiSwitch={uiSwitch}
-            navTitle={navTitle}
-          />
-          {isSidebarOpen ? (
+          {isSidebarOpen && (
             <div
               onClick={closeSidebar}
               className="rspress-sidebar-back-drop"
@@ -122,9 +78,40 @@ export function SideMenu({
                 background: 'rgba(0, 0, 0, 0.6)',
               }}
             />
-          ) : null}
-        </Fragment>
-      ) : null}
-    </Fragment>
+          )}
+        </>
+      )}
+      {uiSwitch?.showAside && (
+        <>
+          <button
+            onClick={() => setIsTocOpen(tocOpened => !tocOpened)}
+            className="flex-center ml-auto"
+            ref={outlineButtonRef}
+          >
+            <span className="text-sm">{outlineTitle}</span>
+            <div
+              className="text-md mr-2"
+              style={{
+                transform: isTocOpen ? 'rotate(90deg)' : 'rotate(0deg)',
+                transition: 'transform 0.2s ease-out',
+                marginTop: '2px',
+              }}
+            >
+              <SvgWrapper icon={ArrowRight} />
+            </div>
+          </button>
+
+          <div
+            className={`rspress-local-toc-container ${isTocOpen ? 'rspress-local-toc-container-show' : ''}`}
+          >
+            <Toc
+              onItemClick={() => {
+                setIsTocOpen(false);
+              }}
+            />
+          </div>
+        </>
+      )}
+    </div>
   );
-}
+};
