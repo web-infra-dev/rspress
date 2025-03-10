c12b3711ffaefc89d50893730b585ddd59bd07ba
SoonIter <sooniter@gmail.com>
2025-03-10 16:14:50 +0800

    chore: update

--- packages/core/src/node/runtimeModule/globalStyles.ts
+++ packages/core/src/node/runtimeModule/globalStyles.ts
@@ -1,23 +1,8 @@
-import fs from 'node:fs/promises';
 import { type FactoryContext, RuntimeModuleID } from '.';
 
 export async function globalStylesVMPlugin(context: FactoryContext) {
   const { config, pluginDriver } = context;
   const globalStylesByPlugins = pluginDriver.globalStyles();
-
-  if (config.globalStyles) {
-    // Only patch user config global styles
-    const source = config.globalStyles;
-    const styleContent = await fs.readFile(source, 'utf-8');
-
-    // Patch --modern-abc, .modern-abc global name
-    const patchedStyleContent = styleContent
-      .replace(/--modern-/g, '--rp-')
-      .replace(/\.modern-doc/g, '.rspress')
-      .replace(/\.modern-/g, '.rspress-');
-
-    await fs.writeFile(source, patchedStyleContent, 'utf-8');
-  }
 
   const moduleContent = (
     await Promise.all(
