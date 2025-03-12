766b429af882dd1be352e9ce149e0a8a61bdd6b9
neverland <chenjiahan.jait@bytedance.com>
2025-03-12 15:50:17 +0800

    test: add case

---
+++ e2e/tests/search-i18n.test.ts
@@ -0,0 +1,50 @@
+import path from 'node:path';
+import { expect, test } from '@playwright/test';
+import { getPort, killProcess, runDevCommand } from '../utils/runCommands';
+import { searchInPage } from '../utils/search';
+
+const fixtureDir = path.resolve(__dirname, '../fixtures');
+
+test.describe('search i18n test', async () => {
+  let appPort;
+  let app;
+
+  test.beforeAll(async () => {
+    const appDir = path.join(fixtureDir, 'search-i18n');
+    appPort = await getPort();
+    app = await runDevCommand(appDir, appPort);
+  });
+
+  test.afterAll(async () => {
+    if (app) {
+      await killProcess(app);
+    }
+  });
+
+  test('should update search index when language changed', async ({ page }) => {
+    await page.goto(`http://localhost:${appPort}`);
+
+    const suggestItems1 = await searchInPage(page, 'Button');
+    expect(await suggestItems1[0].textContent()).toContain('Button En');
+
+    // close the search modal
+    await page.click('body');
+
+    // Switch language to Chinese
+    await page.click('.rspress-nav-menu-group-button');
+    await page.click('.rspress-nav-menu-group-content a');
+    await page.waitForLoadState();
+
+    const suggestItems2 = await searchInPage(page, 'Button');
+    expect(await suggestItems2[0].textContent()).toContain('Button 中文');
+    await page.click('body');
+
+    // Switch language to English
+    await page.click('.rspress-nav-menu-group-button');
+    await page.click('.rspress-nav-menu-group-content a');
+    await page.waitForLoadState();
+
+    const suggestItems3 = await searchInPage(page, 'Button');
+    expect(await suggestItems3[0].textContent()).toContain('Button En');
+  });
+});
