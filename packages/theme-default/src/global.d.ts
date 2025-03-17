23b87d98cb0d52d6648a69393936f2b83dbe3c14
SoonIter <sooniter@gmail.com>
2025-03-17 18:32:13 +0800

    chore: update the .d.ts

--- packages/theme-default/src/global.d.ts
+++ packages/theme-default/src/global.d.ts
@@ -5,11 +5,6 @@ declare module 'virtual-site-data' {
   export default data;
 }
 
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
 declare module '@theme-assets/*' {
   const SvgIcon: React.FC<React.SVGProps<SVGSVGElement>> | string;
   export default SvgIcon;
