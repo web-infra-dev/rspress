2688924e8813fadc615eb8fd0a636b86fe949b86
SoonIter <sooniter@gmail.com>
2025-03-18 12:09:53 +0800

    chore: update

--- packages/theme-default/src/layout/DocLayout/docComponents/a.tsx
+++ packages/theme-default/src/layout/DocLayout/docComponents/a.tsx
@@ -1,6 +1,6 @@
 import { Link } from '@theme';
 import type { ComponentProps } from 'react';
-import styles from './index.module.scss';
+import * as styles from './index.module.scss';
 
 import {
   normalizeHrefInRuntime as normalizeHref,
@@ -83,7 +83,7 @@ export const A = (props: ComponentProps<'a'>) => {
   return (
     <Link
       {...props}
-      className={`${className} ${styles.link} ${styles['inline-link']}`}
+      className={`${className} ${styles.link} ${styles.inlineLink}`}
       href={normalizeLinkHref(href)}
     />
   );
