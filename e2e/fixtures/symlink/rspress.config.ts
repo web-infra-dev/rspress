6e5a3f3e21b9674e17d9ea2eedb81578a6dfdd0a
JounQin <admin@1stg.me>
2025-04-11 15:41:27 +0800

    chore: `builderConfig.tools.rspack.resolve.symlinks: false` cannot be used with pnpm

--- e2e/fixtures/symlink/rspress.config.ts
+++ e2e/fixtures/symlink/rspress.config.ts
@@ -3,15 +3,6 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'doc'),
-  builderConfig: {
-    tools: {
-      rspack: {
-        resolve: {
-          symlinks: false,
-        },
-      },
-    },
-  },
   markdown: {
     mdxRs: false,
   },
