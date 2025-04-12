c634c1e6049dc3d8e061da799dbf1d835c99e23d
LonelySnowman <2438490168@qq.com>
2025-04-12 17:37:46 +0800

    fix: rerun e2e test

--- e2e/tests/plugin-preview.test.ts
+++ e2e/tests/plugin-preview.test.ts
@@ -7,27 +7,15 @@ const fixtureDir = path.resolve(__dirname, '../fixtures');
 test.describe('plugin test', async () => {
   let appPort;
   let app;
-  let customEntryAppPort;
-  let customEntryApp;
   test.beforeAll(async () => {
     const appDir = path.join(fixtureDir, 'plugin-preview');
     appPort = await getPort();
     app = await runDevCommand(appDir, appPort);
-
-    const customEntryAppDir = path.join(
-      fixtureDir,
-      'plugin-preview-custom-entry',
-    );
-    customEntryAppPort = await getPort();
-    customEntryApp = await runDevCommand(customEntryAppDir, customEntryAppPort);
   });
 
   test.afterAll(async () => {
     if (app) {
       await killProcess(app);
-    }
-    if (customEntryApp) {
-      await killProcess(customEntryApp);
     }
   });
 
@@ -56,32 +44,5 @@ test.describe('plugin test', async () => {
     expect(internalDemoCodePreview).toBe('Hello World Internal');
     expect(externalDemoCodePreview).toBe('Hello World External');
     expect(transformedCodePreview).toBe('Render from JSON');
-  });
-
-  test('custom entry Should render the element', async ({ page }) => {
-    await page.goto(`http://localhost:${customEntryApp}/`, {
-      waitUntil: 'networkidle',
-    });
-    const codeBlockElements = await page.$$(
-      '.rspress-doc > div[class*=language-]',
-    );
-
-    const jsxDemoPreview = await page
-      .frameLocator('iframe')
-      .getByText('JSX')
-      .innerText();
-    const tsxDemoPreview = await page
-      .frameLocator('iframe')
-      .getByText('TSX')
-      .innerText();
-    const vueDemoPreview = await page
-      .frameLocator('iframe')
-      .getByText('VUE')
-      .innerText();
-
-    expect(codeBlockElements.length).toBe(3);
-    expect(jsxDemoPreview).toBe('Hello World JSX');
-    expect(tsxDemoPreview).toBe('Hello World TSX');
-    expect(vueDemoPreview).toBe('Hello World VUE');
   });
 });
