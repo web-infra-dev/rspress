8902fcf291fdc789a773a62166a63d443d1ff838
LonelySnowman <2438490168@qq.com>
2025-04-12 17:52:35 +0800

    feat: add plugin preview custom entry e2e test

---
+++ e2e/tests/plugin-preview-custom-entry.test.ts
@@ -0,0 +1,48 @@
+import path from 'node:path';
+import { expect, test } from '@playwright/test';
+import { getPort, killProcess, runDevCommand } from '../utils/runCommands';
+
+const fixtureDir = path.resolve(__dirname, '../fixtures');
+
+test.describe('plugin test', async () => {
+  let appPort;
+  let app;
+  test.beforeAll(async () => {
+    const appDir = path.join(fixtureDir, 'plugin-preview-custom-entry');
+    appPort = await getPort();
+    app = await runDevCommand(appDir, appPort);
+  });
+
+  test.afterAll(async () => {
+    if (app) {
+      await killProcess(app);
+    }
+  });
+
+  test('Should render the element', async ({ page }) => {
+    await page.goto(`http://localhost:${appPort}/`, {
+      waitUntil: 'networkidle',
+    });
+    const codeBlockElements = await page.$$(
+      '.rspress-doc > div[class*=language-]',
+    );
+
+    const internalDemoCodePreview = await page
+      .frameLocator('iframe')
+      .getByText('JSX')
+      .innerText();
+    const externalDemoCodePreview = await page
+      .frameLocator('iframe')
+      .getByText('TSX')
+      .innerText();
+    const transformedCodePreview = await page
+      .frameLocator('iframe')
+      .getByText('VUE')
+      .innerText();
+
+    expect(codeBlockElements.length).toBe(3);
+    expect(internalDemoCodePreview).toBe('Hello World JSX');
+    expect(externalDemoCodePreview).toBe('Hello World TSX');
+    expect(transformedCodePreview).toBe('Hello World VSX');
+  });
+});
