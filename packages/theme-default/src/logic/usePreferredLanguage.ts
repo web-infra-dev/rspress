64402fec4f439c21db3222b213753a2816296c69
zhujiruo <zhujiruo@foxmail.com>
2025-05-22 15:36:32 +0800

    refactor(theme-default): Refactor preferred language hook to support SSG

--- packages/theme-default/src/logic/usePreferredLanguage.ts
+++ packages/theme-default/src/logic/usePreferredLanguage.ts
@@ -6,7 +6,6 @@ import {
 } from '@rspress/runtime';
 import { replaceLang } from '@rspress/shared';
 import { useEffect } from 'react';
-import { useStorageValue } from './useStorageValue';
 
 /**
  * Redirect to preferred locale
@@ -25,39 +24,41 @@ export function usePreferredLanguage() {
   const defaultVersion = siteData.multiVersion.default || '';
   const { base } = siteData;
   const PREFERRED_LANG_KEY = 'rspress-preferred-language';
-  const [preferredLang, setLang] = useStorageValue<string>(
-    PREFERRED_LANG_KEY,
-    '',
-  );
 
   const saveUserPreferredLanguage =
     siteData.themeConfig.saveUserPreferredLanguage ?? 'auto';
 
-  const botRegex = /bot|spider|crawl|lighthouse/i;
-
-  const disableStorage =
-    saveUserPreferredLanguage === 'never' ||
-    !hasMultiLanguage ||
-    // If the request is coming from a bot or crawler, do not redirect.
-    // this ensures that Google's search crawler can work as expected.
-    botRegex.test(window.navigator.userAgent);
+  const checkDisableStorage = () => {
+    const botRegex = /bot|spider|crawl|lighthouse/i;
+    return (
+      saveUserPreferredLanguage === 'never' ||
+      !hasMultiLanguage ||
+      // If the request is coming from a bot or crawler, do not redirect.
+      // this ensures that Google's search crawler can work as expected.
+      botRegex.test(window.navigator.userAgent)
+    );
+  };
+  const getPreferredLanguage = () => {
+    return localStorage.getItem(PREFERRED_LANG_KEY);
+  };
 
   const setPreferredLanguage = (lang: string) => {
-    if (!lang || lang === preferredLang || disableStorage) {
+    const preferredLang = getPreferredLanguage();
+    if (!lang || lang === preferredLang || checkDisableStorage()) {
       return;
     }
-    setLang(lang);
+    localStorage.setItem(PREFERRED_LANG_KEY, lang);
   };
 
   useEffect(() => {
     if (
-      disableStorage ||
+      checkDisableStorage() ||
       (saveUserPreferredLanguage === 'only-home-page' && pageType !== 'home') // only redirect in home page
     ) {
       //  no need to redirect
       return;
     }
-
+    const preferredLang = getPreferredLanguage();
     if (preferredLang && preferredLang !== currentLang) {
       const cleanUrls = siteData.route?.cleanUrls || false;
       const newPath = replaceLang(
@@ -80,7 +81,7 @@ export function usePreferredLanguage() {
   }, []);
 
   return {
-    preferredLang,
+    getPreferredLanguage,
     setPreferredLanguage,
   } as const;
 }
