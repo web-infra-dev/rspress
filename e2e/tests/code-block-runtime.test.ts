2e64ec70c6a348393faf24d47f36a62cb12e9fbd
JounQin <admin@1stg.me>
2025-05-23 14:43:27 +0800

    test: add code-block-runtime e2e case

---
+++ e2e/tests/code-block-runtime.test.ts
@@ -0,0 +1,43 @@
+import path from 'node:path';
+import { expect, test } from '@playwright/test';
+import { getPort, killProcess, runDevCommand } from '../utils/runCommands';
+
+const fixtureDir = path.resolve(__dirname, '../fixtures');
+
+test.describe('<CodeBlockRuntime />', async () => {
+  let appPort;
+  let app;
+  test.beforeAll(async () => {
+    const appDir = path.join(fixtureDir, 'code-block-runtime');
+    appPort = await getPort();
+    app = await runDevCommand(appDir, appPort);
+  });
+
+  test.afterAll(async () => {
+    if (app) {
+      await killProcess(app);
+    }
+  });
+
+  test('Should render title and code with correct language', async ({
+    page,
+  }) => {
+    await page.goto(`http://localhost:${appPort}`, {
+      waitUntil: 'networkidle',
+    });
+    const containers = await page.$$('.language-js');
+    expect(containers.length).toBe(1);
+    const container = containers[0];
+    const title = await container.$('.rspress-code-title');
+    expect(await title!.textContent()).toBe('test.js');
+    const content = await container.$('.rspress-code-content');
+    expect(
+      await content!.evaluate(el => el.classList.contains('rspress-scrollbar')),
+    ).toBe(true);
+    const shikiContainer = await page.waitForSelector('.shiki.css-variables');
+    expect(await shikiContainer.evaluate(el => el.tagName)).toBe('PRE');
+    expect(await shikiContainer.$eval('code', el => el.textContent)).toBe(
+      "console.log('Hello CodeBlockRuntime!')",
+    );
+  });
+});
