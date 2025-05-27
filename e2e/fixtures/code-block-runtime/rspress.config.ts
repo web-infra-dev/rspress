0b7b2037630ae4fb1af3f8e17d59c9fa74dc5d1a
SoonIter <sooniter@gmail.com>
2025-05-27 13:51:57 +0800

    feat(CodeBlockRuntime): add shikiOptions prop

--- e2e/fixtures/code-block-runtime/rspress.config.ts
+++ e2e/fixtures/code-block-runtime/rspress.config.ts
@@ -1,6 +1,12 @@
 import * as path from 'node:path';
+import { transformerNotationHighlight } from '@shikijs/transformers';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
+  markdown: {
+    shiki: {
+      transformers: [transformerNotationHighlight()],
+    },
+  },
 });
