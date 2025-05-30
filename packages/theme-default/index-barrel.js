d56fa9e59d31c62f74bd6c44c11f075027217631
SoonIter <sooniter@gmail.com>
2025-05-30 17:08:22 +0800

    chore: add .js ext

--- packages/theme-default/index-barrel.js
+++ packages/theme-default/index-barrel.js
@@ -1,59 +1,59 @@
 // layout
-export { DocLayout } from './layout/DocLayout';
-export { HomeLayout } from './layout/HomeLayout';
-export { Layout } from './layout/Layout';
-export { NotFoundLayout } from './layout/NotFountLayout';
-export { getCustomMDXComponent } from './layout/DocLayout/docComponents';
+export { DocLayout } from './layout/DocLayout.js';
+export { HomeLayout } from './layout/HomeLayout.js';
+export { Layout } from './layout/Layout.js';
+export { NotFoundLayout } from './layout/NotFountLayout.js';
+export { getCustomMDXComponent } from './layout/DocLayout/docComponents.js';
 
 // logic
-export { usePrevNextPage } from './logic/usePrevNextPage';
-export { useEditLink } from './logic/useEditLink';
-export { useSidebarData } from './logic/useSidebarData';
-export { useHiddenNav, useEnableNav } from './logic/useHiddenNav';
-export { useLocaleSiteData } from './logic/useLocaleSiteData';
+export { usePrevNextPage } from './logic/usePrevNextPage.js';
+export { useEditLink } from './logic/useEditLink.js';
+export { useSidebarData } from './logic/useSidebarData.js';
+export { useHiddenNav, useEnableNav } from './logic/useHiddenNav.js';
+export { useLocaleSiteData } from './logic/useLocaleSiteData.js';
 export {
   useSetup,
   useBindingAsideScroll as bindingAsideScroll,
   scrollToTarget,
-} from './logic/sideEffects';
-export { useFullTextSearch } from './logic/useFullTextSearch';
-export { useRedirect4FirstVisit } from './logic/useRedirect4FirstVisit';
-export { useThemeState } from './logic/useAppearance';
+} from './logic/sideEffects.js';
+export { useFullTextSearch } from './logic/useFullTextSearch.js';
+export { useRedirect4FirstVisit } from './logic/useRedirect4FirstVisit.js';
+export { useThemeState } from './logic/useAppearance.js';
 export {
   renderHtmlOrText,
   parseInlineMarkdownText,
   renderInlineMarkdown,
-} from './logic/utils';
+} from './logic/utils.js';
 
-export * from './components/Search/logic/types';
+export * from './components/Search/logic/types.js';
 
 // components
-export { Aside } from './components/Aside';
-export { Toc } from './components/Toc';
-export { Badge } from './components/Badge';
-export { Button } from './components/Button';
-export { Card } from './components/Card';
-export { CodeBlockRuntime } from './components/CodeBlockRuntime';
-export { DocFooter } from './components/DocFooter';
-export { EditLink } from './components/EditLink';
-export { HomeFeature } from './components/HomeFeature';
-export { HomeFooter } from './components/HomeFooter';
-export { HomeHero } from './components/HomeHero';
-export { LastUpdated } from './components/LastUpdated';
-export { Link } from './components/Link';
-export { LinkCard } from './components/LinkCard';
-export { Nav } from './components/Nav';
-export { Overview } from './components/Overview';
-export { PackageManagerTabs } from './components/PackageManagerTabs';
-export { PrevNextPage } from './components/PrevNextPage';
-export { ScrollToTop } from './components/ScrollToTop';
-export { Search } from './components/Search';
-export { SearchButton } from './components/Search/SearchButton';
-export { SearchPanel } from './components/Search/SearchPanel';
-export { Sidebar, SidebarList } from './components/Sidebar';
-export { SocialLinks } from './components/SocialLinks';
-export { SourceCode } from './components/SourceCode';
-export { Steps } from './components/Steps';
-export { SwitchAppearance } from './components/SwitchAppearance';
-export { Tab, Tabs } from './components/Tabs';
-export { Tag } from './components/Tag';
+export { Aside } from './components/Aside.js';
+export { Toc } from './components/Toc.js';
+export { Badge } from './components/Badge.js';
+export { Button } from './components/Button.js';
+export { Card } from './components/Card.js';
+export { CodeBlockRuntime } from './components/CodeBlockRuntime.js';
+export { DocFooter } from './components/DocFooter.js';
+export { EditLink } from './components/EditLink.js';
+export { HomeFeature } from './components/HomeFeature.js';
+export { HomeFooter } from './components/HomeFooter.js';
+export { HomeHero } from './components/HomeHero.js';
+export { LastUpdated } from './components/LastUpdated.js';
+export { Link } from './components/Link.js';
+export { LinkCard } from './components/LinkCard.js';
+export { Nav } from './components/Nav.js';
+export { Overview } from './components/Overview.js';
+export { PackageManagerTabs } from './components/PackageManagerTabs.js';
+export { PrevNextPage } from './components/PrevNextPage.js';
+export { ScrollToTop } from './components/ScrollToTop.js';
+export { Search } from './components/Search.js';
+export { SearchButton } from './components/Search/SearchButton.js';
+export { SearchPanel } from './components/Search/SearchPanel.js';
+export { Sidebar, SidebarList } from './components/Sidebar.js';
+export { SocialLinks } from './components/SocialLinks.js';
+export { SourceCode } from './components/SourceCode.js';
+export { Steps } from './components/Steps.js';
+export { SwitchAppearance } from './components/SwitchAppearance.js';
+export { Tab, Tabs } from './components/Tabs.js';
+export { Tag } from './components/Tag.js';
