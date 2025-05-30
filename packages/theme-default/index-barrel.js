466cd4e8aae5784072595fb30b2e9b73f18b955b
SoonIter <sooniter@gmail.com>
2025-05-30 17:14:26 +0800

    fix: add index manually

--- packages/theme-default/index-barrel.js
+++ packages/theme-default/index-barrel.js
@@ -1,9 +1,9 @@
 // layout
-export { DocLayout } from './layout/DocLayout.js';
-export { HomeLayout } from './layout/HomeLayout.js';
-export { Layout } from './layout/Layout.js';
-export { NotFoundLayout } from './layout/NotFountLayout.js';
-export { getCustomMDXComponent } from './layout/DocLayout/docComponents.js';
+export { DocLayout } from './layout/DocLayout/index.js';
+export { HomeLayout } from './layout/HomeLayout/index.js';
+export { Layout } from './layout/Layout/index.js';
+export { NotFoundLayout } from './layout/NotFountLayout/index.js';
+export { getCustomMDXComponent } from './layout/DocLayout/docComponents/index.js';
 
 // logic
 export { usePrevNextPage } from './logic/usePrevNextPage.js';
@@ -28,32 +28,35 @@ export {
 export * from './components/Search/logic/types.js';
 
 // components
-export { Aside } from './components/Aside.js';
-export { Toc } from './components/Toc.js';
-export { Badge } from './components/Badge.js';
-export { Button } from './components/Button.js';
-export { Card } from './components/Card.js';
-export { CodeBlockRuntime } from './components/CodeBlockRuntime.js';
-export { DocFooter } from './components/DocFooter.js';
-export { EditLink } from './components/EditLink.js';
-export { HomeFeature } from './components/HomeFeature.js';
-export { HomeFooter } from './components/HomeFooter.js';
-export { HomeHero } from './components/HomeHero.js';
-export { LastUpdated } from './components/LastUpdated.js';
-export { Link } from './components/Link.js';
-export { LinkCard } from './components/LinkCard.js';
-export { Nav } from './components/Nav.js';
-export { Overview } from './components/Overview.js';
-export { PackageManagerTabs } from './components/PackageManagerTabs.js';
-export { PrevNextPage } from './components/PrevNextPage.js';
-export { ScrollToTop } from './components/ScrollToTop.js';
-export { Search } from './components/Search.js';
+export { Aside } from './components/Aside/index.js';
+export { Toc } from './components/Toc/index.js';
+export { Badge } from './components/Badge/index.js';
+export { Button } from './components/Button/index.js';
+export { Card } from './components/Card/index.js';
+export { CodeBlockRuntime } from './components/CodeBlockRuntime/index.js';
+export { DocFooter } from './components/DocFooter/index.js';
+export { EditLink } from './components/EditLink/index.js';
+export { HomeFeature } from './components/HomeFeature/index.js';
+export { HomeFooter } from './components/HomeFooter/index.js';
+export { HomeHero } from './components/HomeHero/index.js';
+export { LastUpdated } from './components/LastUpdated/index.js';
+export { Link } from './components/Link/index.js';
+export { LinkCard } from './components/LinkCard/index.js';
+export { Nav } from './components/Nav/index.js';
+export { Overview } from './components/Overview/index.js';
+export { PackageManagerTabs } from './components/PackageManagerTabs/index.js';
+export { PrevNextPage } from './components/PrevNextPage/index.js';
+export { ScrollToTop } from './components/ScrollToTop/index.js';
+export { Search } from './components/Search/index.js';
 export { SearchButton } from './components/Search/SearchButton.js';
 export { SearchPanel } from './components/Search/SearchPanel.js';
-export { Sidebar, SidebarList } from './components/Sidebar.js';
-export { SocialLinks } from './components/SocialLinks.js';
-export { SourceCode } from './components/SourceCode.js';
-export { Steps } from './components/Steps.js';
-export { SwitchAppearance } from './components/SwitchAppearance.js';
-export { Tab, Tabs } from './components/Tabs.js';
-export { Tag } from './components/Tag.js';
+export {
+  Sidebar,
+  SidebarList,
+} from './components/Sidebar/index.js';
+export { SocialLinks } from './components/SocialLinks/index.js';
+export { SourceCode } from './components/SourceCode/index.js';
+export { Steps } from './components/Steps/index.js';
+export { SwitchAppearance } from './components/SwitchAppearance/index.js';
+export { Tab, Tabs } from './components/Tabs/index.js';
+export { Tag } from './components/Tag/index.js';
