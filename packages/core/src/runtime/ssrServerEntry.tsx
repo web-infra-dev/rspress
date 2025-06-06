e829ee16d2e7c0c26d3ac2bbf0d52780210c7c38
SoonIter <sooniter@gmail.com>
2025-06-06 14:32:28 +0800

    perf(ssg): for renderToString oom

--- packages/core/src/runtime/ssrServerEntry.tsx
+++ packages/core/src/runtime/ssrServerEntry.tsx
@@ -2,12 +2,30 @@ import { DataContext, ThemeContext } from '@rspress/runtime';
 import { StaticRouter } from '@rspress/runtime/server';
 import type { PageData } from '@rspress/shared';
 import { type Unhead, UnheadProvider } from '@unhead/react/server';
-import { renderToString } from 'react-dom/server';
+import { renderToPipeableStream } from 'react-dom/server';
 
+import { PassThrough } from 'node:stream';
+import { text } from 'node:stream/consumers';
+import type { ReactNode } from 'react';
 import { App } from './App';
 import { initPageData } from './initPageData';
 
 const DEFAULT_THEME = 'light';
+
+function renderToHtml(app: ReactNode): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const passThrough = new PassThrough();
+    const { pipe } = renderToPipeableStream(app, {
+      onError(error) {
+        reject(error);
+      },
+      onAllReady() {
+        pipe(passThrough);
+        text(passThrough).then(resolve, reject);
+      },
+    });
+  });
+}
 
 export async function render(
   pagePath: string,
@@ -15,7 +33,7 @@ export async function render(
 ): Promise<{ appHtml: string; pageData: PageData }> {
   const initialPageData = await initPageData(pagePath);
 
-  const appHtml = renderToString(
+  const appHtml = await renderToHtml(
     <ThemeContext.Provider value={{ theme: DEFAULT_THEME }}>
       <DataContext.Provider value={{ data: initialPageData }}>
         <StaticRouter location={pagePath}>
