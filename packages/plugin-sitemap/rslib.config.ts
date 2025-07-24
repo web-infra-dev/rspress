96c4aa2a87a322368f74493db0e64c34b6eeff3d
SoonIter <sooniter@gmail.com>
2025-07-24 20:14:04 +0800

    feat(plugin-sitemap): init @rspress/plugin-sitemap

---
+++ packages/plugin-sitemap/rslib.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@rslib/core';
+import { pluginPublint } from 'rsbuild-plugin-publint';
+
+export default defineConfig({
+  plugins: [pluginPublint()],
+  lib: [
+    { bundle: true, syntax: 'es2022', format: 'esm', dts: { bundle: true } },
+  ],
+});
