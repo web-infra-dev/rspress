75faee0949fe7b647da826eb906f17c753aaaa36
SoonIter <sooniter@gmail.com>
2025-07-28 14:40:57 +0800

    test: update no loose case

---
+++ e2e/fixtures/check-dead-link/rspress-no-loose.config.ts
@@ -0,0 +1,33 @@
+import * as path from 'node:path';
+import { defineConfig } from '@rspress/core';
+
+export default defineConfig({
+  root: path.join(__dirname, 'doc'),
+  lang: 'zh',
+  base: '/base',
+  markdown: {
+    link: {
+      loosePrefix: false,
+    },
+  },
+  themeConfig: {
+    darkMode: false,
+    localeRedirect: 'never',
+    locales: [
+      {
+        lang: 'zh',
+        title: '一个很棒的项目',
+        description: '一个很棒的项目描述',
+        // 语言切换按钮的文案
+        // Language switch button text
+        label: '简体中文',
+      },
+      {
+        lang: 'en',
+        title: 'A awesome project',
+        description: 'A awesome project description',
+        label: 'English',
+      },
+    ],
+  },
+});
