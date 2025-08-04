ec163c86c03958baed1029da353c0e610c1d7218
SoonIter <sooniter@gmail.com>
2025-08-04 15:54:25 +0800

    chore: update

--- packages/plugin-llms/src/runtime/LlmsViewOptions.tsx
+++ packages/plugin-llms/src/runtime/LlmsViewOptions.tsx
@@ -163,9 +163,9 @@ const LlmsViewOptions = ({
             <g
               fill="none"
               stroke="black"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
             >
               <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
               <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
@@ -173,7 +173,7 @@ const LlmsViewOptions = ({
           </svg>
         ),
         onClick: () => {
-          return navigator.clipboard.writeText(fullMarkdownUrl);
+          navigator.clipboard.writeText(fullMarkdownUrl);
         },
       },
       chatgpt: {
