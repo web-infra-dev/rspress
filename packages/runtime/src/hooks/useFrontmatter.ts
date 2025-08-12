52a3eeca82f17dba0473bb57a04f95a77364dfc4
Soon <sooniter@gmail.com>
2025-08-12 11:28:22 +0800

    refactor(theme): usePageData => usePage (#2466)

--- packages/runtime/src/hooks/useFrontmatter.ts
+++ packages/runtime/src/hooks/useFrontmatter.ts
@@ -1,8 +1,8 @@
 import type { FrontMatterMeta } from '@rspress/shared';
-import { usePageData } from './usePageData';
+import { usePage } from './usePage';
 
 export const useFrontmatter = (): { frontmatter: FrontMatterMeta } => {
-  const { page } = usePageData();
+  const { page } = usePage();
   const { frontmatter = {} } = page;
   return { frontmatter };
 };
