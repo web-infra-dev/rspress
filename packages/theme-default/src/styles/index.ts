13025f181415c6ec920cbe9b5bc71a609ae03fd2
sooniter <sooniter@gmail.com>
2025-09-22 22:07:02 +0800

    chore: complete the doclayout

--- packages/theme-default/src/styles/index.ts
+++ packages/theme-default/src/styles/index.ts
@@ -8,7 +8,6 @@ import './vars.css';
 import './base.css';
 import './tailwind.css';
 import './code.scss';
-import './doc.scss';
 import './scrollbar.scss';
 import './shiki.scss';
 import './container.scss';
