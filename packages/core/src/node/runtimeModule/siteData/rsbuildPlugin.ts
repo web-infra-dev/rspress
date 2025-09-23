75c8b1d7a6bd4c0938f29abf12b8cb02211bbedd
SoonIter <sooniter@gmail.com>
2025-09-23 19:38:00 +0800

    chore: update

--- packages/core/src/node/runtimeModule/siteData/rsbuildPlugin.ts
+++ packages/core/src/node/runtimeModule/siteData/rsbuildPlugin.ts
@@ -6,7 +6,7 @@ import { createSiteData } from './createSiteData';
 export const siteDataVMPlugin: VirtualModulePlugin = context => {
   const { config, pluginDriver } = context;
   return {
-    [RuntimeModuleID.SiteData]: async function ({ addDependency }) {
+    [RuntimeModuleID.SiteData]: async ({ addDependency }) => {
       const force = !pluginDriver.haveNavSidebarConfig;
       const metaFileSet = new Set<string>();
       const mdFileSet = new Set<string>();
