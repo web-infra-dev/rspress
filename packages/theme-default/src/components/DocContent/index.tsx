f17b5b5c2d2647f92ed22e629096fbd90a09c129
SoonIter <sooniter@gmail.com>
2025-09-25 15:27:01 +0800

    feat: add FallbackingTitle

--- packages/theme-default/src/components/DocContent/index.tsx
+++ packages/theme-default/src/components/DocContent/index.tsx
@@ -1,41 +1,32 @@
 import { MDXProvider } from '@mdx-js/react';
 import { Content, usePage, useSite } from '@rspress/runtime';
 import { getCustomMDXComponent } from '@theme';
-import { slug } from 'github-slugger';
-import { A } from '../../components/DocContent/docComponents/a';
-import { H1 } from '../../components/DocContent/docComponents/title';
 import './doc.scss';
+import { FallbackHeading } from './FallbackHeading';
 
-function FallbackTitle() {
+export function FallbackTitle() {
   const { site } = useSite();
   const { page } = usePage();
   const { headingTitle, title } = page;
-  const titleSlug = title && slug(title);
 
   return (
     site.themeConfig.fallbackHeadingTitle !== false &&
-    !headingTitle &&
-    titleSlug && (
-      <H1 id={titleSlug}>
-        {title}
-        <A className="header-anchor" href={`#${titleSlug}`} aria-hidden>
-          #
-        </A>
-      </H1>
-    )
+    !headingTitle && <FallbackHeading level={1} title={title} />
   );
 }
 
 export function DocContent({
   components,
+  isOverviewPage = false,
 }: {
   components: Record<string, React.FC<any>> | undefined;
+  isOverviewPage?: boolean;
 }) {
   const mdxComponents = { ...getCustomMDXComponent(), ...components };
 
   return (
     <MDXProvider components={mdxComponents}>
-      <FallbackTitle />
+      {!isOverviewPage && <FallbackTitle />}
       <Content />
     </MDXProvider>
   );
