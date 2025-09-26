b5040fa7b4c45754cc59f35647ce9dba82fe90c9
SoonIter <sooniter@gmail.com>
2025-09-26 16:33:32 +0800

    chore: update BEM

--- packages/theme-default/src/components/Steps/index.tsx
+++ packages/theme-default/src/components/Steps/index.tsx
@@ -1,12 +1,6 @@
 import type { ReactNode } from 'react';
-import * as styles from './index.module.scss';
+import './index.scss';
 
 export function Steps({ children }: { children: ReactNode }) {
-  return (
-    <div
-      className={`rp-ml-4 rp-mb-11 rp-border-l rp-pl-6 ${styles.rspressSteps} [counter-reset:step]`}
-    >
-      {children}
-    </div>
-  );
+  return <div className="rp-steps">{children}</div>;
 }
