04a4848f25308cf5c2c92edab1f3f018ee2375a5
SoonIter <sooniter@gmail.com>
2025-09-28 20:50:24 +0800

    chore: upgrade Aside SidebarMenu

--- packages/theme-default/src/components/SidebarMenu/useClickOutside.ts
+++ packages/theme-default/src/components/SidebarMenu/useClickOutside.ts
@@ -19,9 +19,11 @@ function useClickOutside(
       handler(event);
       return;
     };
-    document.addEventListener('click', listener);
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
     return () => {
-      document.removeEventListener('click', listener);
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
     };
   }, [ref, handler]);
 }
