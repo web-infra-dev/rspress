1e6558e28dafa3cf4f55e68612e30f4fb0d75600
SoonIter <sooniter@gmail.com>
2025-09-28 17:44:56 +0800

    chore: upgrade Aside SidebarMenu

--- packages/theme-default/src/components/SidebarMenu/useClickOutside.ts
+++ packages/theme-default/src/components/SidebarMenu/useClickOutside.ts
@@ -10,22 +10,18 @@ function useClickOutside(
     const listener = (event: MouseEvent | TouchEvent) => {
       const refs = Array.isArray(ref) ? ref : [ref];
 
-      if (Array.isArray(refs)) {
-        for (const r of refs) {
-          const { current: el } = r;
-          if (el && el.contains(event.target as Node)) {
-            return;
-          }
+      for (const r of refs) {
+        const { current: el } = r;
+        if (el && el.contains(event.target as Node)) {
+          return;
         }
-        handler(event);
-        return;
       }
+      handler(event);
+      return;
     };
-    document.addEventListener('mousedown', listener);
-    document.addEventListener('touchstart', listener);
+    document.addEventListener('click', listener);
     return () => {
-      document.removeEventListener('mousedown', listener);
-      document.removeEventListener('touchstart', listener);
+      document.removeEventListener('click', listener);
     };
   }, [ref, handler]);
 }
