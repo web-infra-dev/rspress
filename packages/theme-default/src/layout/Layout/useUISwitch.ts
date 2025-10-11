de46522168f579bab56c55d79f6841ef73ee5c8a
SoonIter <sooniter@gmail.com>
2025-10-11 17:58:42 +0800

    chore!: remove uiSwitch

--- packages/theme-default/src/layout/Layout/useUISwitch.ts
+++ packages/theme-default/src/layout/Layout/useUISwitch.ts
@@ -1,16 +1,4 @@
-import {
-  useLocaleSiteData,
-  useLocation,
-  usePageData,
-  useWindowSize,
-} from '@rspress/runtime';
-import { createContext, useContext, useEffect, useState } from 'react';
-import { useEnableNav, useHiddenNav } from '../../logic/useHiddenNav';
-
-enum QueryStatus {
-  Show = '1',
-  Hide = '0',
-}
+import { createContext, useContext } from 'react';
 
 export interface UISwitchResult {
   showNavbar: boolean;
@@ -18,8 +6,6 @@ export interface UISwitchResult {
   showSidebarMenu: boolean;
   showAside: boolean;
   showDocFooter: boolean;
-  navbarHeight: number;
-  sidebarMenuHeight: number;
 }
 
 export const UISwitchContext = createContext({} as UISwitchResult);
@@ -30,99 +16,16 @@ export function useUISwitch(): UISwitchResult {
 }
 
 export function useCreateUISwitch(): UISwitchResult {
-  const { page, siteData } = usePageData();
-  const { frontmatter } = page;
-  const { themeConfig } = siteData;
-  const localesData = useLocaleSiteData();
-  const location = useLocation();
-  const isOverviewPage = frontmatter?.overview ?? false;
-  const getShowAside = () => {
-    // if in iframe, default value is false
-    const defaultHasAside =
-      typeof window === 'undefined' ? true : window.top === window.self;
-    return (
-      (frontmatter?.outline ?? themeConfig?.outline ?? defaultHasAside) &&
-      !isOverviewPage
-    );
-  };
-  const [showNavbar, setShowNavbar] = useEnableNav();
-  const hiddenNav = useHiddenNav();
-  const [showAside, setShowAside] = useState(getShowAside());
-  const [showDocFooter, setShowDocFooter] = useState(
-    frontmatter?.footer ?? true,
-  );
-
-  const sidebar = localesData.sidebar || {};
   // sidebar Priority
   // 1. frontmatter.sidebar
   // 2. themeConfig.locales.sidebar
   // 3. themeConfig.sidebar
-  const showSidebar =
-    frontmatter?.sidebar !== false && Object.keys(sidebar).length > 0;
 
-  const { width } = useWindowSize();
-
-  const showSidebarMenu =
-    width <= 960 || (width <= 1280 && page.toc.length > 0);
-
-  useEffect(() => {
-    setShowAside(getShowAside());
-  }, [page, siteData]);
-
-  // Control the display of the navbar, sidebar and aside
-  useEffect(() => {
-    const query = new URLSearchParams(location.search);
-    const documentStyle = document.documentElement.style;
-    const originalSidebarWidth =
-      documentStyle.getPropertyValue('--rp-sidebar-width');
-    const originalAsideWidth =
-      documentStyle.getPropertyValue('--rp-aside-width');
-    const originNavbar = showNavbar;
-    const originDocFooter = showDocFooter;
-    const navbar = query.get('navbar');
-    const sidebar = query.get('sidebar');
-    const aside = query.get('outline');
-    const footer = query.get('footer');
-
-    if (navbar === QueryStatus.Hide) {
-      setShowNavbar(false);
-    }
-
-    if (sidebar === QueryStatus.Hide) {
-      document.documentElement.style.setProperty('--rp-sidebar-width', '0px');
-    }
-
-    if (aside === QueryStatus.Hide) {
-      document.documentElement.style.setProperty('--rp-aside-width', '0px');
-    }
-
-    if (footer === QueryStatus.Hide) {
-      setShowDocFooter(false);
-    }
-
-    return () => {
-      document.documentElement.style.setProperty(
-        '--rp-sidebar-width',
-        originalSidebarWidth,
-      );
-      document.documentElement.style.setProperty(
-        '--rp-aside-width',
-        originalAsideWidth,
-      );
-      setShowNavbar(originNavbar);
-      setShowDocFooter(originDocFooter);
-    };
-  }, [location.search]);
-
-  const navbarHeight = hiddenNav ? 0 : width <= 768 ? 56 : 72;
-  const sidebarMenuHeight = showSidebarMenu ? 46 : 0;
   return {
-    showAside,
-    showNavbar,
-    showSidebar,
-    showSidebarMenu,
-    showDocFooter,
-    navbarHeight,
-    sidebarMenuHeight,
+    showNavbar: true,
+    showSidebar: true,
+    showSidebarMenu: true,
+    showAside: true,
+    showDocFooter: true,
   };
 }
