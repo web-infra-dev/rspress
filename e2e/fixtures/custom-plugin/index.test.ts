0ce6bdef3cdc501b5c5fe9ae1c30cbe5cdab9a7c
SoonIter <sooniter@gmail.com>
2025-10-11 17:34:46 +0800

    test(e2e): upgrade test e2e

--- e2e/fixtures/custom-plugin/index.test.ts
+++ e2e/fixtures/custom-plugin/index.test.ts
@@ -2,8 +2,8 @@ import { expect, test } from '@playwright/test';
 import { getPort, killProcess, runDevCommand } from '../../utils/runCommands';
 
 test.describe('plugin test', async () => {
-  let appPort;
-  let app;
+  let appPort: number;
+  let app: Awaited<ReturnType<typeof runDevCommand>>;
   test.beforeAll(async () => {
     const appDir = __dirname;
     appPort = await getPort();
@@ -20,10 +20,8 @@ test.describe('plugin test', async () => {
     await page.goto(`http://localhost:${appPort}/filepath-route`, {
       waitUntil: 'networkidle',
     });
-    const h1 = await page.getByRole('heading', {
-      name: /Demo1/,
-    });
-    await expect(h1).toBeTruthy();
+    const heading = page.getByRole('heading', { name: /Demo1/ });
+    await expect(heading).toBeVisible();
   });
 
   test('Should add route by content', async ({ page }) => {
@@ -31,9 +29,7 @@ test.describe('plugin test', async () => {
       waitUntil: 'networkidle',
     });
 
-    const h1 = await page.getByRole('heading', {
-      name: /Demo2/,
-    });
-    await expect(h1).toBeTruthy();
+    const heading = page.getByRole('heading', { name: /Demo2/ });
+    await expect(heading).toBeVisible();
   });
 });
