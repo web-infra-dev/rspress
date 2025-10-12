2d3b4b62ab9732d14b74131b1a8972fc17e2f9a5
sooniter <sooniter@gmail.com>
2025-10-13 04:16:40 +0800

    chore: update e2e

--- e2e/fixtures/replace-rules/index.test.ts
+++ e2e/fixtures/replace-rules/index.test.ts
@@ -2,8 +2,8 @@ import { expect, test } from '@playwright/test';
 import { getPort, killProcess, runDevCommand } from '../../utils/runCommands';
 
 test.describe('replace-rules test', async () => {
-  let appPort;
-  let app;
+  let appPort: number;
+  let app: Awaited<ReturnType<typeof runDevCommand>>;
   test.beforeAll(async () => {
     const appDir = __dirname;
     appPort = await getPort();
@@ -20,20 +20,22 @@ test.describe('replace-rules test', async () => {
     await page.goto(`http://localhost:${appPort}`);
 
     // replace text in _meta.json
-    const nav = page.locator('.rp-nav-menu');
-    await expect(nav).toHaveText('bar-meta');
+    const navItem = page.locator('.rp-nav-menu__item__container').nth(0);
+    await expect(navItem).toHaveText('bar-meta');
 
     // replace text in object frontmatter
-    const hero = page.locator('h1');
+    const hero = page.locator('.rp-home-hero__title-brand');
     await expect(hero).toHaveText('bar-hero');
   });
 
   test('Foo page', async ({ page }) => {
     await page.goto(`http://localhost:${appPort}/foo`);
+    const doc = page.locator('.rspress-doc');
+    await expect(doc).toBeVisible();
 
     // text in string frontmatter
-    const title = page.locator('title');
-    await expect(title).toHaveText('bar-title');
+    const title = await page.$('title');
+    expect(await title?.textContent()).toBe('bar-title');
 
     // replace text in shared mdx content
     const h2 = page.locator('h2');
--- e2e/fixtures/search-i18n/rspress.config.ts
+++ e2e/fixtures/search-i18n/rspress.config.ts
@@ -15,5 +15,6 @@ export default defineConfig({
         label: 'English',
       },
     ],
+    localeRedirect: 'never',
   },
 });
