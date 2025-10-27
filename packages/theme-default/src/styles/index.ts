c68fd41b0ff793f5875770dc1f4e3f2fa6899f0d
SoonIter <sooniter@gmail.com>
2025-10-27 20:13:16 +0800

    docs: upgrade codeblock css vars

--- packages/theme-default/src/styles/index.ts
+++ packages/theme-default/src/styles/index.ts
@@ -1,9 +1,9 @@
 /**
  * The theme code will be bundled by Modern.js Module Tools, but it will ignore the `@import` in css file. So we need to import the css file in js/ts file to make sure the css file will be bundled.
  */
-import './vars/brand-vars.scss';
-import './vars/shiki-vars.scss';
-import './vars/code-vars.scss';
+import './vars/brand-vars.css';
+import './vars/shiki-vars.css';
+import './vars/code-vars.css';
 import './vars.css';
 
 import './base.css';
