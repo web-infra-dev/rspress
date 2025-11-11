aad86a9d90bd6ae070852b44002c180bc40ecd0f
Soon <sooniter@gmail.com>
2025-11-11 12:04:04 +0800

    fix(plugin-preview): @theme module not found regression (#2740)

--- e2e/tests/plugin-preview.test.ts
+++ e2e/tests/plugin-preview.test.ts
@@ -1,6 +1,12 @@
 import path from 'node:path';
 import { expect, test } from '@playwright/test';
-import { getPort, killProcess, runDevCommand } from '../utils/runCommands';
+import {
+  getPort,
+  killProcess,
+  runBuildCommand,
+  runDevCommand,
+  runPreviewCommand,
+} from '../utils/runCommands';
 
 const fixtureDir = path.resolve(__dirname, '../fixtures');
 
@@ -70,5 +76,48 @@ test.describe('plugin test', async () => {
       'Hello World External',
     );
     expect(innerCodePreviewWithPreviewModeInternal).toBe('This is a component');
+  });
+});
+
+test.describe('plugin preview build', async () => {
+  let appPort: number;
+  let app;
+  test.beforeAll(async () => {
+    const appDir = path.join(fixtureDir, 'plugin-preview');
+    appPort = await getPort();
+    await runBuildCommand(appDir);
+    app = await runPreviewCommand(appDir, appPort);
+  });
+
+  test.afterAll(async () => {
+    if (app) {
+      await killProcess(app);
+    }
+  });
+  test('build should work', async ({ page }) => {
+    await page.goto(`http://localhost:${appPort}`, {
+      waitUntil: 'networkidle',
+    });
+    const codeBlockElements = await page.$$(
+      '.rspress-doc > div[class*=language-]',
+    );
+
+    const internalDemoCodePreview = await page
+      .frameLocator('iframe')
+      .getByText('Internal')
+      .innerText();
+    const externalDemoCodePreview = await page
+      .frameLocator('iframe')
+      .getByText('External')
+      .innerText();
+    const transformedCodePreview = await page
+      .frameLocator('iframe')
+      .getByText('JSON')
+      .innerText();
+
+    expect(codeBlockElements.length).toBe(3);
+    expect(internalDemoCodePreview).toBe('Hello World Internal');
+    expect(externalDemoCodePreview).toBe('Hello World External');
+    expect(transformedCodePreview).toBe('Render from JSON');
   });
 });
