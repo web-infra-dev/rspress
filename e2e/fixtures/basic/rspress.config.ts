f31a31a4292e806273a19596dc470357419fab1f
SoonIter <sooniter@gmail.com>
2025-11-11 15:40:47 +0800

    fix: e2e

--- e2e/fixtures/basic/rspress.config.ts
+++ e2e/fixtures/basic/rspress.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         icon: 'github',
         mode: 'dom',
         content:
-          '<a href="/zh"><img src="https://assets.rspack.rs/rspress/rspress-logo.svg" /></a>',
+          '<a href="/zh"><img src="https://assets.rspack.rs/rspress/rspress-logo.svg" style="width:300px;" /></a>',
       },
     ],
   },
