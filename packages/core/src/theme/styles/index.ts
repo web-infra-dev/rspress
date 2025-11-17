cef72c74a1160002dd4197a643dd225eca85c6ce
SoonIter <sooniter@gmail.com>
2025-11-17 19:29:15 +0800

    chore: upgrade css

--- packages/core/src/theme/styles/index.ts
+++ packages/core/src/theme/styles/index.ts
@@ -1,6 +1,7 @@
 /**
  * The theme code will be bundled by Modern.js Module Tools, but it will ignore the `@import` in css file. So we need to import the css file in js/ts file to make sure the css file will be bundled.
  */
+import 'nprogress/nprogress.css';
 import './vars/brand-vars.css';
 import './vars/shiki-vars.css';
 import './vars/code-vars.css';
@@ -10,4 +11,3 @@ import './vars/base-vars.css';
 import './base.css';
 import './scrollbar.scss';
 import './shiki.scss';
-import 'nprogress/nprogress.css';
