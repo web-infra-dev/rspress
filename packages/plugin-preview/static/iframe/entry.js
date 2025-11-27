8bf06451d31109dc9c4d9e402fbeea5e43ec7b05
SoonIter <sooniter@gmail.com>
2025-11-27 19:05:34 +0800

    fix: update

--- packages/plugin-preview/static/iframe/entry.js
+++ packages/plugin-preview/static/iframe/entry.js
@@ -4,11 +4,14 @@ function setDocumentTheme(isDark) {
   if (isDark) {
     document.documentElement.classList.add('dark');
     document.documentElement.style.colorScheme = 'dark';
-    localStorage.setItem('rspress-plugin-preview-theme-appearance', 'dark');
   } else {
     document.documentElement.classList.remove('dark');
     document.documentElement.style.colorScheme = 'light';
   }
+  localStorage.setItem(
+    'rspress-plugin-preview-theme-appearance',
+    isDark ? 'dark' : 'light',
+  );
 }
 
 const saved = localStorage.getItem(storageKey) || 'light';
