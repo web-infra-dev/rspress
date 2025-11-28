4976b6d4acd36f2677ad367ec2c24909b9a049fe
JounQin <admin@1stg.me>
2025-11-28 16:03:40 +0800

    fix: render `beforeNavMenu` correctly

--- packages/core/src/theme/layout/Layout/index.tsx
+++ packages/core/src/theme/layout/Layout/index.tsx
@@ -115,6 +115,7 @@ export function Layout(props: LayoutProps) {
     afterHero,
     beforeFeatures,
     afterFeatures,
+    beforeNavMenu,
     afterNavMenu,
     components,
     HomeLayout = DefaultHomeLayout,
@@ -222,6 +223,7 @@ export function Layout(props: LayoutProps) {
             beforeNavTitle={beforeNavTitle}
             afterNavTitle={afterNavTitle}
             navTitle={navTitle}
+            beforeNavMenu={beforeNavMenu}
             afterNavMenu={afterNavMenu}
           />
           {afterNav}
