f2bd89b60658e40ebfb7981d836cdaa7e711efe5
Timeless0911 <1604889533@qq.com>
2025-12-02 15:30:44 +0800

    feat: disable Rspack native watcher by default

--- packages/core/src/node/initRsbuild.ts
+++ packages/core/src/node/initRsbuild.ts
@@ -342,11 +342,11 @@ async function createInternalBuildConfig(
           })
           .end();
 
-        chain.experiments({
-          // Enable native watcher by default unless RSPRESS_NATIVE_WATCHER is set to 'false'
-          ...chain.get('experiments'),
-          nativeWatcher: process.env.RSPRESS_NATIVE_WATCHER !== 'false',
-        });
+        // chain.experiments({
+        //   // Enable native watcher by default unless RSPRESS_NATIVE_WATCHER is set to 'false'
+        //   ...chain.get('experiments'),
+        //   nativeWatcher: process.env.RSPRESS_NATIVE_WATCHER !== 'false',
+        // });
 
         if (chain.plugins.has(CHAIN_ID.PLUGIN.REACT_FAST_REFRESH)) {
           chain.plugin(CHAIN_ID.PLUGIN.REACT_FAST_REFRESH).tap(options => {
