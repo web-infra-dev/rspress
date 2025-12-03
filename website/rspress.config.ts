fe58389e69e41e08ee0165b3dd30db61cbd7d4cd
SoonIter <sooniter@gmail.com>
2025-12-03 15:22:10 +0800

    chore: test

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
       verificationContent: '8F5BFE50E65777F1',
     }),
     pluginOg({
-      domain: siteUrl,
+      domain: 'https://deploy-preview-2835--rspress-v2.netlify.app',
     }),
   ],
   builderConfig: {
