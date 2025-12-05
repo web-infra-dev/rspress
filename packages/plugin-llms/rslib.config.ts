073068ce39474a324d114ea8b96b80cc9b800191
Timeless0911 <1604889533@qq.com>
2025-12-05 17:23:06 +0800

    chore: update

--- packages/plugin-llms/rslib.config.ts
+++ packages/plugin-llms/rslib.config.ts
@@ -1,6 +1,6 @@
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSass } from '@rsbuild/plugin-sass';
-import { defineConfig } from '@rslib/core';
+import { defineConfig, rspack } from '@rslib/core';
 import { pluginPublint } from 'rsbuild-plugin-publint';
 
 export default defineConfig({
@@ -15,8 +15,16 @@ export default defineConfig({
           'runtime/index': './src/runtime/index.tsx',
         },
       },
-      banner: {
-        js: 'import "./index.css"',
+      tools: {
+        rspack: {
+          plugins: [
+            new rspack.BannerPlugin({
+              banner: 'import "./index.css";',
+              raw: true,
+              include: /runtime\/index.js$/,
+            }),
+          ],
+        },
       },
       bundle: true,
       format: 'esm',
