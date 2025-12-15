53aa4ecd77b2e035327b37fa0cd667fbb5ac6766
SoonIter <sooniter@gmail.com>
2025-12-15 21:22:00 +0800

    chore: update

--- packages/core/src/node/theme/loader.ts
+++ packages/core/src/node/theme/loader.ts
@@ -2,12 +2,18 @@ import type { Rspack } from '@rsbuild/core';
 import { exportStarOptimizerTransform } from './exportStarOptimizerTransform.js';
 
 export default async function themeIndexFileOptimizerLoader(
-  this: Rspack.LoaderContext<{}>,
+  this: Rspack.LoaderContext<null>,
   source: string,
 ) {
   this.cacheable(true);
   const callback = this.async();
   const filepath = this.resourcePath;
   const content = await exportStarOptimizerTransform(source, filepath);
+
+  if (process.env.DEBUG === 'RSPRESS_EXPORT_STAR') {
+    console.log(
+      `--- RSPRESS_EXPORT_STAR DEBUG ---\nFile: ${filepath}\nTransformed Content:\n${content}\n--- END DEBUG ---`,
+    );
+  }
   callback(null, content);
 }
