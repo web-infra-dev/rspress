85ccb2eab01295735c32495f699ff1c52ce1a819
SoonIter <sooniter@gmail.com>
2025-12-17 11:21:16 +0800

    chore: update ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   // exclude webpack / rspack self-feature test cases when run rspack / webpack test
   testDir: './e2e',
-  // 1 hour for all tests
-  globalTimeout: 60 * 60 * 1000,
-  // 3 min for each test
-  timeout: 3 * 60 * 1000,
+  // 30 min for all tests
+  globalTimeout: 30 * 60 * 1000,
+  // 1 min for each test
+  timeout: 60 * 1000,
   quiet: true,
   reporter: 'list',
   use: {
@@ -14,5 +14,5 @@ export default defineConfig({
     video: 'on',
     viewport: { width: 1440, height: 900 }, // screen size
   },
-  retries: process.env.CI ? 5 : 0,
+  retries: process.env.CI ? 3 : 0,
 });
