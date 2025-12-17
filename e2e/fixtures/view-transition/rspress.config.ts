2de9231165093375e9c52e765be5946ad7642478
Soon <sooniter@gmail.com>
2025-12-17 14:45:40 +0800

    chore: remove view transition (#2898)

--- e2e/fixtures/view-transition/rspress.config.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'node:path';
-import { defineConfig } from '@rspress/core';
-
-export default defineConfig({
-  themeConfig: {
-    enableContentAnimation: true,
-  },
-  root: path.join(__dirname, 'doc'),
-});
