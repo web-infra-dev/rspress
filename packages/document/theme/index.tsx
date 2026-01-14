1f9ad2c7326bacec424771165f6cd00b1e1cdad8
Soon <sooniter@gmail.com>
2026-01-14 20:09:31 +0800

    docs(v1): add V2 banner to guide users to new documentation (#3004)

--- packages/document/theme/index.tsx
+++ packages/document/theme/index.tsx
@@ -2,14 +2,17 @@ import { NavIcon } from '@rstack-dev/doc-ui/nav-icon';
 import Theme from 'rspress/theme';
 import { HomeLayout as BasicHomeLayout } from 'rspress/theme';
 import { ToolStack } from './components/ToolStack';
+import { V2Banner } from './components/V2Banner';
 import './index.css';
 
 function HomeLayout() {
-  return <BasicHomeLayout afterFeatures={<ToolStack />} />;
+  return (
+    <BasicHomeLayout afterFeatures={<ToolStack />} beforeNav={<V2Banner />} />
+  );
 }
 
 const Layout = () => {
-  return <Theme.Layout beforeNavTitle={<NavIcon />} />;
+  return <Theme.Layout beforeNavTitle={<NavIcon />} beforeNav={<V2Banner />} />;
 };
 
 export default {
