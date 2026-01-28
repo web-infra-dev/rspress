0abd3794606406f25ae2bbfeae3ee94f1aecdc0c
neverland <chenjiahan.jait@bytedance.com>
2026-01-28 10:33:28 +0800

    fix

--- packages/core/tests/config/esm-async/rspress.config.ts
+++ packages/core/tests/config/esm-async/rspress.config.ts
@@ -1,9 +1,14 @@
 import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default async () => {
-  await fs.readFile(import.meta.filename, 'utf-8');
+  await fs.readFile(__filename, 'utf-8');
   return {
-    root: import.meta.dirname,
+    root: __dirname,
     title: 'my-title',
   };
 };
