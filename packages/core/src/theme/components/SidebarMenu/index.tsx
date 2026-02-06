9e54ff780fea3b7d51d19baa619346444c00c1b5
Soon <sooniter@gmail.com>
2026-02-06 15:23:11 +0800

    fix(theme/outline): prevent sidebar menu text and icons from overflowing on narrow screens and upgrade @rslib/core (#3108)

--- packages/core/src/theme/components/SidebarMenu/index.tsx
+++ packages/core/src/theme/components/SidebarMenu/index.tsx
@@ -104,17 +104,20 @@ export const SidebarMenu = forwardRef(
             >
               {scrolledHeader?.text ? (
                 <span
-                  className="rp-doc"
+                  className="rp-sidebar-menu__right__text rp-doc"
                   {...renderInlineMarkdown(scrolledHeader.text)}
                 />
               ) : (
-                <span>{t('outlineTitle')}</span>
+                <span className="rp-sidebar-menu__right__text">
+                  {t('outlineTitle')}
+                </span>
               )}
               <ReadPercent size={14} strokeWidth={2} />
               {/* TODO: discussion */}
               {headers.length !== 0 && (
                 <SvgWrapper
                   icon={IconArrowRight}
+                  className="rp-sidebar-menu__right__icon"
                   style={{
                     transform: isOutlineOpen ? 'rotate(90deg)' : 'rotate(0deg)',
                     transition: 'transform 0.2s ease-out',
