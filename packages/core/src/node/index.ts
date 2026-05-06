f6d02c1fb48e6dea85d4c737bc31d71391b222b9
JounQin <admin@1stg.me>
2026-05-06 17:27:32 +0800

    chore: export `remarkImage` for reusing like `remarkLink` (#3353)

--- packages/core/src/node/index.ts
+++ packages/core/src/node/index.ts
@@ -1,5 +1,6 @@
 export { build } from './build';
 export { dev } from './dev';
 export { remarkFileCodeBlock } from './mdx/remarkPlugins/fileCodeBlock';
+export { remarkImage } from './mdx/remarkPlugins/image';
 export { remarkLink } from './mdx/remarkPlugins/link';
 export { serve } from './serve';
