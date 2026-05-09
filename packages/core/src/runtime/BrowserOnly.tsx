431d688f082630c50f2ed20f41c9a882c66d74ac
SoonIter <sooniter@gmail.com>
2026-05-09 15:50:39 +0800

    chore: update

--- packages/core/src/runtime/BrowserOnly.tsx
+++ packages/core/src/runtime/BrowserOnly.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export interface BrowserOnlyProps {
   children: () => ReactNode | Promise<ReactNode>;
@@ -9,39 +9,42 @@ export interface BrowserOnlyProps {
 type BrowserOnlyState =
   | {
       status: 'pending';
+      children: BrowserOnlyProps['children'];
     }
   | {
       status: 'resolved';
+      children: BrowserOnlyProps['children'];
       content: ReactNode;
     }
   | {
       status: 'rejected';
+      children: BrowserOnlyProps['children'];
       error: unknown;
     };
 
 export function BrowserOnly(props: BrowserOnlyProps) {
   const { children, fallback = null } = props;
-  const childrenRef = useRef(children);
   const [state, setState] = useState<BrowserOnlyState>({
     status: 'pending',
+    children,
   });
-
-  childrenRef.current = children;
 
   useEffect(() => {
     let isMounted = true;
 
+    setState({ status: 'pending', children });
+
     Promise.resolve()
-      .then(() => childrenRef.current())
+      .then(() => children())
       .then(
         content => {
           if (isMounted) {
-            setState({ status: 'resolved', content });
+            setState({ status: 'resolved', children, content });
           }
         },
         error => {
           if (isMounted) {
-            setState({ status: 'rejected', error });
+            setState({ status: 'rejected', children, error });
           }
         },
       );
@@ -49,7 +52,11 @@ export function BrowserOnly(props: BrowserOnlyProps) {
     return () => {
       isMounted = false;
     };
-  }, []);
+  }, [children]);
+
+  if (state.children !== children) {
+    return fallback;
+  }
 
   if (state.status === 'rejected') {
     throw state.error;
