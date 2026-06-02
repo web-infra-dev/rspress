b60a1c27e15729fea8511ae7c9929168cd7d3dfb
Soon <sooniter@gmail.com>
2026-06-02 20:50:23 +0800

    revert(theme): remove image preload support (#3432)

--- packages/core/src/theme/components/DocContent/docComponents/img.tsx
+++ packages/core/src/theme/components/DocContent/docComponents/img.tsx
@@ -1,14 +1,6 @@
 import { normalizeImagePath } from '@rspress/core/runtime';
 import type { ComponentProps } from 'react';
-import { Image } from '@rspress/core/theme';
 
 export const Img = (props: ComponentProps<'img'>) => {
-  const preload = props.loading === 'eager' && props.fetchPriority !== 'low';
-  return (
-    <Image
-      {...props}
-      src={normalizeImagePath(props.src || '')}
-      preload={preload}
-    />
-  );
+  return <img {...props} src={normalizeImagePath(props.src || '')} />;
 };
