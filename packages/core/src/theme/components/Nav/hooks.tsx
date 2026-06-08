5fc9e6d0cf503c55c2aa1f6de2d123cdb034aeb3
Soon <sooniter@gmail.com>
2026-06-08 17:47:50 +0800

    fix(theme/Nav): render active language as non-link (#3450)

--- packages/core/src/theme/components/Nav/hooks.tsx
+++ packages/core/src/theme/components/Nav/hooks.tsx
@@ -79,22 +79,26 @@ export function useLangsMenu() {
   const translationMenuData = hasMultiLanguage
     ? {
         items: localeLanguages.map(item => {
+          const isCurrentLang = item.lang === currentLang;
+
           return {
             text: item?.label,
-            link: replaceLang(
-              pathname + search,
-              {
-                current: currentLang,
-                target: item.lang,
-                default: defaultLang,
-              },
-              {
-                current: currentVersion,
-                default: defaultVersion,
-              },
-              cleanUrls,
-              pageType === '404',
-            ),
+            link: isCurrentLang
+              ? ''
+              : replaceLang(
+                  pathname + search,
+                  {
+                    current: currentLang,
+                    target: item.lang,
+                    default: defaultLang,
+                  },
+                  {
+                    current: currentVersion,
+                    default: defaultVersion,
+                  },
+                  cleanUrls,
+                  pageType === '404',
+                ),
             lang: item.lang,
             rel: 'alternate',
           };
