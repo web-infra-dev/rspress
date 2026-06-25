5d665d93d4f5553940aec13824b574da13cead43
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2026-06-25 22:11:59 +0530

    fix: organize imports

--- packages/core/src/theme/components/DocContent/docComponents/a.tsx
+++ packages/core/src/theme/components/DocContent/docComponents/a.tsx
@@ -1,6 +1,6 @@
+import { useSite } from '@rspress/core/runtime';
 import { Link } from '@rspress/core/theme';
 import { startTransition, type ComponentProps } from 'react';
-import { useSite } from '@rspress/core/runtime';
 
 export const A = (props: ComponentProps<'a'>) => {
   const siteData = useSite();
