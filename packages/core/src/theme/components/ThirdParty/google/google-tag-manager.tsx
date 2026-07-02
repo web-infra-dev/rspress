8e5dcbfe1402bdc797599e8fc706ba6fdb55ef8e
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2026-07-03 00:59:06 +0530

    feat(google-tag-manager): add Google Tag Manager component for enhanced tracking and dataLayer management

---
+++ packages/core/src/theme/components/ThirdParty/google/google-tag-manager.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { Script } from '@rspress/core/theme';
+
+type JSONValue =
+  string | number | boolean | JSONValue[] | { [key: string]: JSONValue };
+
+type GTMParamsBaseParams = {
+  dataLayer?: { [key: string]: JSONValue };
+  dataLayerName?: string;
+  auth?: string;
+  preview?: string;
+  nonce?: string;
+};
+
+type GTMParamsWithId = GTMParamsBaseParams & {
+  gtmId: string;
+  gtmScriptUrl?: string;
+};
+
+type GTMParamsWithScriptUrl = GTMParamsBaseParams & {
+  gtmId?: string;
+  gtmScriptUrl: string;
+};
+
+declare global {
+  interface Window {
+    dataLayer?: Object[];
+    [key: string]: any;
+  }
+}
+
+export type GTMParams = GTMParamsWithId | GTMParamsWithScriptUrl;
+
+let currDataLayerName = 'dataLayer';
+
+export function GoogleTagManager(props: GTMParams) {
+  const {
+    gtmId,
+    gtmScriptUrl,
+    dataLayerName = 'dataLayer',
+    auth,
+    preview,
+    dataLayer,
+    nonce,
+  } = props;
+
+  currDataLayerName = dataLayerName;
+
+  const scriptUrl = new URL(
+    gtmScriptUrl || 'https://www.googletagmanager.com/gtm.js',
+  );
+  if (gtmId) {
+    scriptUrl.searchParams.set('id', gtmId);
+  }
+  if (dataLayerName !== 'dataLayer') {
+    scriptUrl.searchParams.set('l', dataLayerName);
+  }
+  if (auth) {
+    scriptUrl.searchParams.set('gtm_auth', auth);
+  }
+  if (preview) {
+    scriptUrl.searchParams.set('gtm_preview', preview);
+    scriptUrl.searchParams.set('gtm_cookies_win', 'x');
+  }
+
+  return (
+    <>
+      <Script
+        id="_rspress-gtm-init"
+        dangerouslySetInnerHTML={{
+          __html: `
+      (function(w,l){
+        w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+        ${dataLayer ? `w[l].push(${JSON.stringify(dataLayer)})` : ''}
+      })(window,'${dataLayerName}');`,
+        }}
+        nonce={nonce}
+      />
+      <Script id="_rspress-gtm" src={scriptUrl.href} nonce={nonce} />
+    </>
+  );
+}
--- packages/core/src/theme/components/ThirdParty/google/index.tsx
+++ packages/core/src/theme/components/ThirdParty/google/index.tsx
@@ -1,2 +1,4 @@
+export { GoogleAnalytics, type GAParams } from './google-analytics';
 export { YouTubeEmbed, type YouTubeEmbedTypes } from './youtube-embed';
 export { GoogleMapsEmbed, type GoogleMapsEmbedTypes } from './google-maps';
+export { GoogleTagManager, type GTMParams } from './google-tag-manager';
