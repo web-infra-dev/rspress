7fac2548e9d394ed4150c39e829e3aa1847a8935
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2026-07-10 22:27:15 +0530

    feat(plugin-algolia): include TA_LOCALES in exported locales

--- packages/plugin-algolia/src/runtime/index.ts
+++ packages/plugin-algolia/src/runtime/index.ts
@@ -1,2 +1,2 @@
-export { type Locales, RU_LOCALES, ZH_LOCALES } from './locales';
+export { type Locales, RU_LOCALES, ZH_LOCALES, TA_LOCALES } from './locales';
 export { Search, type SearchProps } from './Search';
