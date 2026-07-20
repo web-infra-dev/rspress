eecf28d72c48548823b8e3ea00a3e1b0e14c6e27
Soon <sooniter@gmail.com>
2026-07-20 16:50:46 +0800

    chore(ci/rsdoctor): add auto-nav-sidebar baseline to Rsdoctor bundle diff analysis (#3332)

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -35,6 +35,19 @@ const commonRsdoctorConfig = {
   },
 };
 
+const getRsdoctorReportType = (environmentName?: string) => {
+  switch (environmentName) {
+    case 'node':
+      return 'html';
+    case 'node_md':
+      return 'md';
+    case 'web':
+      return 'js';
+    default:
+      return environmentName ?? 'unknown';
+  }
+};
+
 export default defineConfig({
   title: 'Rspress',
   description: 'Rsbuild based static site generator',
@@ -158,7 +171,7 @@ export default defineConfig({
               ...commonRsdoctorConfig,
               output: {
                 ...commonRsdoctorConfig.output,
-                reportDir: `./doc_build/diff-rsdoctor/${config.name}`,
+                reportDir: `./doc_build/diff-rsdoctor/website_${getRsdoctorReportType(config.name)}`,
               },
             }),
           );
