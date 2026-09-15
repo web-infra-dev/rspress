6c492b079c817a944e687fdb7852a19d79a2b054
Soon <sooniter@gmail.com>
2026-09-15 17:40:36 +0800

    fix(theme/Search): use native prefetch for search index (#3681)

--- packages/core/src/theme/components/Search/logic/providers/LocalProvider.ts
+++ packages/core/src/theme/components/Search/logic/providers/LocalProvider.ts
@@ -44,6 +44,23 @@ type FlexSearchDocumentWithType = Document<
   false
 >;
 
+export function getSearchIndexURL(
+  lang: string,
+  version: string,
+): string | undefined {
+  const searchIndexGroupID = `${version ?? ''}###${lang ?? ''}`;
+  const searchIndexHashValue = searchIndexHash[searchIndexGroupID];
+
+  // for example, in page-type-home fixture, there is only home index.md, so no search index generated
+  if (!searchIndexHashValue) {
+    return undefined;
+  }
+
+  const searchIndexVersion = version ? `.${version.replace('.', '_')}` : '';
+  const searchIndexLang = lang ? `.${lang}` : '';
+  return `${removeTrailingSlash(__WEBPACK_PUBLIC_PATH__)}/static/${SEARCH_INDEX_NAME}${searchIndexVersion}${searchIndexLang}.${searchIndexHashValue}.json`;
+}
+
 const cjkRegex =
   /[\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]|[\u4E00-\u9FCC\u3400-\u4DB5\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34\udf40-\udfff]|\ud86e[\udc00-\udc1d]|[\u3041-\u3096]|[\u30A1-\u30FA]/giu;
 const cyrillicRegex = /[\u0400-\u04FF]/g;
@@ -70,16 +87,10 @@ export class LocalProvider implements Provider {
   #fetchPromise?: Promise<PageIndexInfo[]>;
 
   async #getPages(lang: string, version: string): Promise<PageIndexInfo[]> {
-    const searchIndexGroupID = `${version ?? ''}###${lang ?? ''}`;
-
-    // for example, in page-type-home fixture, there is only home index.md, so no search index generated
-    if (!searchIndexHash[searchIndexGroupID]) {
+    const searchIndexURL = getSearchIndexURL(lang, version);
+    if (!searchIndexURL) {
       return [];
     }
-
-    const searchIndexVersion = version ? `.${version.replace('.', '_')}` : '';
-    const searchIndexLang = lang ? `.${lang}` : '';
-    const searchIndexURL = `${removeTrailingSlash(__WEBPACK_PUBLIC_PATH__)}/static/${SEARCH_INDEX_NAME}${searchIndexVersion}${searchIndexLang}.${searchIndexHash[searchIndexGroupID]}.json`;
 
     const handleError = (result: unknown) => {
       console.error(
